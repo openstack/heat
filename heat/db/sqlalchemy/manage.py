c2ab4a39d136949db5d2e6c6c2b90930929fe087
Zane Bitter <zbitter@redhat.com>
2012-04-17 17:38:48 +0200

    Remove hard-coded paths from DB setup script
    Since versions of heat can and will change, we do not want want them
    hard-coded in the DB setup script. Instead, use the Python import logic to
    determine the location of the module.
    
    This will use the local copy of heat (which is usually what you want) if
    run from the top level of the repository. Otherwise it will use the
    installed version, as determined by Python.
    
    Signed-off-by: Zane Bitter <zbitter@redhat.com>

--- heat/db/sqlalchemy/manage.py
+++ heat/db/sqlalchemy/manage.py
@@ -3,8 +3,12 @@ from migrate.versioning.shell import main
 import migrate.exceptions
 
 if __name__ == '__main__':
+    import os.path
+    migrate_repo_path = os.path.join(os.path.dirname(__file__),
+                                     'migrate_repo')
+
     try:
         main(url='mysql://heat:heat@localhost/heat', debug='False',
-             repository='migrate_repo')
+             repository=migrate_repo_path)
     except migrate.exceptions.DatabaseAlreadyControlledError:
         print 'Database already version controlled.'
