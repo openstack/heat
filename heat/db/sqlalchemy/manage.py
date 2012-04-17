d4fda0273580fc8575298614bc90569a73c1d5b3
Zane Bitter <zbitter@redhat.com>
2012-04-17 17:38:48 +0200

    Avoid printing exception in db-setup
    If the db-setup is run multiple times without dropping the database, the
    attempt to set up version control again causes an exception. Since this is
    an expected condition, just print a message and exit normally.
    
    Signed-off-by: Zane Bitter <zbitter@redhat.com>

--- heat/db/sqlalchemy/manage.py
+++ heat/db/sqlalchemy/manage.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 from migrate.versioning.shell import main
+import migrate.exceptions
 
 if __name__ == '__main__':
-    main(url='mysql://heat:heat@localhost/heat', debug='False',
-         repository='migrate_repo')
+    try:
+        main(url='mysql://heat:heat@localhost/heat', debug='False',
+             repository='migrate_repo')
+    except migrate.exceptions.DatabaseAlreadyControlledError:
+        print 'Database already version controlled.'
