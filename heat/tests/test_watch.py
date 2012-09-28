ebc3129e44d5762756d28133880b07a13f6e0be3
Steven Hardy <shardy@redhat.com>
2012-09-28 08:02:20 +0100

    heat engine : Move watch logic into WatchRule class
    Move engine/manager.py logic into WatchRule class so watch-
    related DB manipulation and related logic is encapsulated in
    the WatchRule class
    
    Fixes #217
    
    Change-Id: I5405ab631de17efda7eefb45dadad55ee12c533c
    Signed-off-by: Steven Hardy <shardy@redhat.com>

--- heat/tests/test_watch.py
+++ heat/tests/test_watch.py
@@ -21,6 +21,8 @@ from nose import with_setup
 import unittest
 from nose.exc import SkipTest
 import logging
+from heat.common import context
+import heat.db as db_api
 
 from heat.openstack.common import timeutils
 try:
@@ -39,10 +41,24 @@ class WatchData:
                                      'Unit': 'Count'}}
 
 
+@attr(tag=['unit', 'watchrule'])
+@attr(speed='fast')
 class WatchRuleTest(unittest.TestCase):
 
-    @attr(tag=['unit', 'watchrule'])
-    @attr(speed='fast')
+    def setUp(self):
+        self.username = 'watchrule_test_user'
+
+        self.m = mox.Mox()
+
+        self.ctx = context.get_admin_context()
+        self.m.StubOutWithMock(self.ctx, 'username')
+        self.ctx.username = self.username
+
+        self.m.ReplayAll()
+
+    def tearDown(self):
+        self.m.UnsetStubs()
+
     def test_minimum(self):
         rule = {
         'EvaluationPeriods': '1',
@@ -58,19 +74,27 @@ class WatchRuleTest(unittest.TestCase):
         data.append(WatchData(53, now - datetime.timedelta(seconds=150)))
 
         # all > 50 -> NORMAL
-        watcher = watchrule.WatchRule(rule, data, last, now)
+        watcher = watchrule.WatchRule(context=self.ctx,
+                                      watch_name="testwatch",
+                                      rule=rule,
+                                      stack_name="teststack",
+                                      watch_data=data,
+                                      last_evaluated=last)
         new_state = watcher.get_alarm_state()
         logger.info(new_state)
         self.assertEqual(new_state, 'NORMAL')
 
         data.append(WatchData(25, now - datetime.timedelta(seconds=250)))
-        watcher = watchrule.WatchRule(rule, data, last, now)
+        watcher = watchrule.WatchRule(context=self.ctx,
+                                      watch_name="testwatch",
+                                      rule=rule,
+                                      stack_name="teststack",
+                                      watch_data=data,
+                                      last_evaluated=last)
         new_state = watcher.get_alarm_state()
         logger.info(new_state)
         self.assertEqual(new_state, 'ALARM')
 
-    @attr(tag=['unit', 'watchrule'])
-    @attr(speed='fast')
     def test_maximum(self):
         rule = {
         'EvaluationPeriods': '1',
@@ -86,19 +110,29 @@ class WatchRuleTest(unittest.TestCase):
         data.append(WatchData(23, now - datetime.timedelta(seconds=150)))
 
         # all < 30 -> NORMAL
-        watcher = watchrule.WatchRule(rule, data, last, now)
+        watcher = watchrule.WatchRule(context=self.ctx,
+                                      watch_name="testwatch",
+                                      rule=rule,
+                                      stack_name="teststack",
+                                      watch_data=data,
+                                      last_evaluated=last)
+        watcher.now = now
         new_state = watcher.get_alarm_state()
         logger.info(new_state)
         self.assertEqual(new_state, 'NORMAL')
 
         data.append(WatchData(35, now - datetime.timedelta(seconds=150)))
-        watcher = watchrule.WatchRule(rule, data, last, now)
+        watcher = watchrule.WatchRule(context=self.ctx,
+                                      watch_name="testwatch",
+                                      rule=rule,
+                                      stack_name="teststack",
+                                      watch_data=data,
+                                      last_evaluated=last)
+        watcher.now = now
         new_state = watcher.get_alarm_state()
         logger.info(new_state)
         self.assertEqual(new_state, 'ALARM')
 
-    @attr(tag=['unit', 'watchrule'])
-    @attr(speed='fast')
     def test_samplecount(self):
 
         rule = {
@@ -115,14 +149,26 @@ class WatchRuleTest(unittest.TestCase):
         data.append(WatchData(1, now - datetime.timedelta(seconds=150)))
 
         # only 2 samples -> NORMAL
-        watcher = watchrule.WatchRule(rule, data, last, now)
+        watcher = watchrule.WatchRule(context=self.ctx,
+                                      watch_name="testwatch",
+                                      rule=rule,
+                                      stack_name="teststack",
+                                      watch_data=data,
+                                      last_evaluated=last)
+        watcher.now = now
         new_state = watcher.get_alarm_state()
         logger.info(new_state)
         self.assertEqual(new_state, 'NORMAL')
 
         # only 3 samples -> ALARM
         data.append(WatchData(1, now - datetime.timedelta(seconds=200)))
-        watcher = watchrule.WatchRule(rule, data, last, now)
+        watcher = watchrule.WatchRule(context=self.ctx,
+                                      watch_name="testwatch",
+                                      rule=rule,
+                                      stack_name="teststack",
+                                      watch_data=data,
+                                      last_evaluated=last)
+        watcher.now = now
         new_state = watcher.get_alarm_state()
         logger.info(new_state)
         self.assertEqual(new_state, 'ALARM')
@@ -130,13 +176,17 @@ class WatchRuleTest(unittest.TestCase):
         # only 3 samples (one old) -> NORMAL
         data.pop(0)
         data.append(WatchData(1, now - datetime.timedelta(seconds=400)))
-        watcher = watchrule.WatchRule(rule, data, last, now)
+        watcher = watchrule.WatchRule(context=self.ctx,
+                                      watch_name="testwatch",
+                                      rule=rule,
+                                      stack_name="teststack",
+                                      watch_data=data,
+                                      last_evaluated=last)
+        watcher.now = now
         new_state = watcher.get_alarm_state()
         logger.info(new_state)
         self.assertEqual(new_state, 'NORMAL')
 
-    @attr(tag=['unit', 'watchrule'])
-    @attr(speed='fast')
     def test_sum(self):
         rule = {
         'EvaluationPeriods': '1',
@@ -152,20 +202,30 @@ class WatchRuleTest(unittest.TestCase):
         data.append(WatchData(23, now - datetime.timedelta(seconds=150)))
 
         # all < 40 -> NORMAL
-        watcher = watchrule.WatchRule(rule, data, last, now)
+        watcher = watchrule.WatchRule(context=self.ctx,
+                                      watch_name="testwatch",
+                                      rule=rule,
+                                      stack_name="teststack",
+                                      watch_data=data,
+                                      last_evaluated=last)
+        watcher.now = now
         new_state = watcher.get_alarm_state()
         logger.info(new_state)
         self.assertEqual(new_state, 'NORMAL')
 
         # sum > 100 -> ALARM
         data.append(WatchData(85, now - datetime.timedelta(seconds=150)))
-        watcher = watchrule.WatchRule(rule, data, last, now)
+        watcher = watchrule.WatchRule(context=self.ctx,
+                                      watch_name="testwatch",
+                                      rule=rule,
+                                      stack_name="teststack",
+                                      watch_data=data,
+                                      last_evaluated=last)
+        watcher.now = now
         new_state = watcher.get_alarm_state()
         logger.info(new_state)
         self.assertEqual(new_state, 'ALARM')
 
-    @attr(tag=['unit', 'watchrule'])
-    @attr(speed='fast')
     def test_ave(self):
         rule = {
         'EvaluationPeriods': '1',
@@ -180,13 +240,85 @@ class WatchRuleTest(unittest.TestCase):
         data = [WatchData(117, now - datetime.timedelta(seconds=100))]
         data.append(WatchData(23, now - datetime.timedelta(seconds=150)))
 
-        watcher = watchrule.WatchRule(rule, data, last, now)
+        watcher = watchrule.WatchRule(context=self.ctx,
+                                      watch_name="testwatch",
+                                      rule=rule,
+                                      stack_name="teststack",
+                                      watch_data=data,
+                                      last_evaluated=last)
+        watcher.now = now
         new_state = watcher.get_alarm_state()
         logger.info(new_state)
         self.assertEqual(new_state, 'NORMAL')
 
         data.append(WatchData(195, now - datetime.timedelta(seconds=250)))
-        watcher = watchrule.WatchRule(rule, data, last, now)
+        watcher = watchrule.WatchRule(context=self.ctx,
+                                      watch_name="testwatch",
+                                      rule=rule,
+                                      stack_name="teststack",
+                                      watch_data=data,
+                                      last_evaluated=last)
+        watcher.now = now
         new_state = watcher.get_alarm_state()
         logger.info(new_state)
         self.assertEqual(new_state, 'ALARM')
+
+    def test_load(self):
+        # Insert two dummy watch rules into the DB
+        values = {'stack_name': u'wordpress_ha', 'state': 'NORMAL',
+                  'name': u'HttpFailureAlarm',
+                   'rule': {
+                        u'EvaluationPeriods': u'1',
+                        u'AlarmActions': [u'WebServerRestartPolicy'],
+                        u'AlarmDescription': u'Restart the WikiDatabase',
+                        u'Namespace': u'system/linux',
+                        u'Period': u'300',
+                        u'ComparisonOperator': u'GreaterThanThreshold',
+                        u'Statistic': u'SampleCount',
+                        u'Threshold': u'2',
+                        u'MetricName': u'ServiceFailure'}}
+        db_ret = db_api.watch_rule_create(self.ctx, values)
+        self.assertNotEqual(db_ret, None)
+        values['name'] = 'AnotherWatch'
+        db_ret = db_api.watch_rule_create(self.ctx, values)
+        self.assertNotEqual(db_ret, None)
+
+        # Then use WatchRule.load() to retrieve each by name
+        # and check that the object properties match the data above
+        for wn in ('HttpFailureAlarm', 'AnotherWatch'):
+            wr = watchrule.WatchRule.load(self.ctx, wn)
+            self.assertEqual(type(wr), watchrule.WatchRule)
+            self.assertEqual(wr.name, wn)
+            self.assertEqual(wr.state, values['state'])
+            self.assertEqual(wr.rule, values['rule'])
+            self.assertEqual(wr.stack_name, values['stack_name'])
+            self.assertEqual(wr.timeperiod, datetime.timedelta(
+                             seconds=int(values['rule']['Period'])))
+
+        # Cleanup
+        db_api.watch_rule_delete(self.ctx, 'HttpFailureAlarm')
+        db_api.watch_rule_delete(self.ctx, 'AnotherWatch')
+
+    def test_store(self):
+        rule = {u'EvaluationPeriods': u'1',
+                 u'AlarmActions': [u'WebServerRestartPolicy'],
+                 u'AlarmDescription': u'Restart the WikiDatabase',
+                 u'Namespace': u'system/linux',
+                 u'Period': u'300',
+                 u'ComparisonOperator': u'GreaterThanThreshold',
+                 u'Statistic': u'SampleCount',
+                 u'Threshold': u'2',
+                 u'MetricName': u'ServiceFailure'}
+        wr = watchrule.WatchRule(context=self.ctx, watch_name='storetest',
+                                 rule=rule, stack_name='teststack')
+        wr.store()
+
+        dbwr = db_api.watch_rule_get_by_name(self.ctx, 'storetest')
+        self.assertNotEqual(dbwr, None)
+        self.assertEqual(dbwr.name, 'storetest')
+        self.assertEqual(dbwr.state, watchrule.WatchRule.NORMAL)
+        self.assertEqual(dbwr.stack_name, 'teststack')
+        self.assertEqual(dbwr.rule, rule)
+
+        # Cleanup
+        db_api.watch_rule_delete(self.ctx, 'storetest')
