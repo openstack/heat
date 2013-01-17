d8093014b6b181ca48cc22df9723088d673d5389
Steven Hardy <shardy@redhat.com>
2013-01-17 10:30:54 +0000

    heat tests : remove debug print
    Remove SHDEBUG line which I accidentally left in test_identifier.py
    
    Change-Id: Ic965958e28b1c825408ce5ff44ced1398663bcb0
    Signed-off-by: Steven Hardy <shardy@redhat.com>

--- heat/tests/test_identifier.py
+++ heat/tests/test_identifier.py
@@ -316,7 +316,6 @@ class IdentifierTest(unittest.TestCase):
                   'stacks%2F%253A%2525%2F%253A%252F%2F%253A'])
         url = self.url_prefix + enc_arn
         hi = identifier.HeatIdentifier.from_arn_url(url)
-        print "SHDEBUG hi.arn()=%s" % hi.arn()
         hi2 = identifier.HeatIdentifier.from_arn_url(self.url_prefix +
                                                      hi.arn_url_path())
         self.assertEqual(hi, hi2)
