25dfc5f062b5c8a9f9266173607c199931b4c07e
Thomas Herve <therve@gmail.com>
2013-04-29 17:21:42 +0200

    Validate properties against the schema in validate_template
    Change-Id: I81457d8d363226b90b2a878ec2c93f88b376dc83
    Fixes: bug #1154722

--- heat/engine/service.py
+++ heat/engine/service.py
@@ -28,6 +28,7 @@ from heat.common import exception
 from heat.common import identifier
 from heat.engine import parameters
 from heat.engine import parser
+from heat.engine import properties
 from heat.engine import resource
 from heat.engine import resources  # pyflakes_bypass review 23102
 from heat.engine import watchrule
@@ -286,6 +287,13 @@ class EngineService(service.Service):
             if not res.get('Type'):
                 return {'Error':
                         'Every Resources object must contain a Type member.'}
+            ResourceClass = resource.get_class(res['Type'])
+            props = properties.Properties(ResourceClass.properties_schema,
+                                          res.get('Properties', {}))
+            try:
+                props.validate(with_value=False)
+            except Exception as ex:
+                return {'Error': str(ex)}
 
         tmpl_params = parser.Parameters(None, tmpl)
         format_validate_parameter = lambda p: dict(p.schema)
