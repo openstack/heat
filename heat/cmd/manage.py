126bf9f28caf5e9015844b3d8acffb9abf77873c
Angus Salkeld <asalkeld@redhat.com>
2013-05-27 21:06:31 +1000

    Add a basic heat-manage
    The heat/cmd/manage.py structure is copied from glance to give
    us some consistency.
    
    Removing unused cliutils ...
    
    blueprint heat-manage
    Change-Id: If3b69eb4c3672740515fd308ec868af1cd3c9396

---
+++ heat/cmd/manage.py
@@ -0,0 +1,76 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+  CLI interface for heat management.
+"""
+
+import sys
+
+from oslo.config import cfg
+
+from heat.db import api as db_api
+from heat.db import migration
+from heat.openstack.common import log
+from heat import version
+
+
+CONF = cfg.CONF
+
+
+def do_db_version():
+    """Print database's current migration level."""
+    print migration.db_version()
+
+
+def do_db_sync():
+    """
+    Place a database under migration control and upgrade,
+    creating first if necessary.
+    """
+    migration.db_sync(CONF.command.version)
+
+
+def add_command_parsers(subparsers):
+    parser = subparsers.add_parser('db_version')
+    parser.set_defaults(func=do_db_version)
+
+    parser = subparsers.add_parser('db_sync')
+    parser.set_defaults(func=do_db_sync)
+    parser.add_argument('version', nargs='?')
+    parser.add_argument('current_version', nargs='?')
+
+
+command_opt = cfg.SubCommandOpt('command',
+                                title='Commands',
+                                help='Available commands',
+                                handler=add_command_parsers)
+
+
+def main():
+    CONF.register_cli_opt(command_opt)
+    try:
+        CONF(sys.argv[1:], project='heat',
+             version=version.version_info.version_string())
+        log.setup("heat")
+        db_api.configure()
+    except RuntimeError as e:
+        sys.exit("ERROR: %s" % e)
+
+    try:
+        CONF.command.func()
+    except Exception as e:
+        sys.exit("ERROR: %s" % e)
