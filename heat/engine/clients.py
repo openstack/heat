openstack/heat

commit f3fc5c579b720f42db256bf6ccaa9aa15e848f92

No longer pass no_cache=True to novaclient create
The default is now os_cache=False, so default behaviour will not
use a keyring that prompts for a password.

This was committed on 2012-12-12 and released in
python-novaclient 2.11.0 on 2013-02-11

Part of blueprint auth-token-only

Change-Id: Id2846b2e344f8e3321240795aebf9a59ddad711a

heat/engine/clients.py
@@ -95,18 +95,7 @@ class OpenStackClients(object):
             logger.error("Nova connection failed, no password or auth_token!")
             return None
 
-        client = None
-        try:
-            # Workaround for issues with python-keyring, need no_cache=True
-            # ref https://bugs.launchpad.net/python-novaclient/+bug/1020238
-            # TODO(shardy): May be able to remove when the bug above is fixed
-            client = novaclient.Client(1.1, no_cache=True, **args)
-            self._nova[service_type] = client
-        except TypeError:
-            # for compatibility with essex, which doesn't have no_cache=True
-            # TODO(shardy): remove when we no longer support essex
-            client = novaclient.Client(1.1, **args)
-            self._nova[service_type] = client
+        client = novaclient.Client(1.1, **args)
 
         if con.password is None and con.auth_token is not None:
             management_url = self.url_for(service_type=service_type)

tools/pip-requires
@@ -19,7 +19,7 @@ kombu>=1.0.4
 argparse
 lxml>=2.3,<=2.3.5
 sqlalchemy-migrate>=0.7.2
-python-novaclient
+python-novaclient>=2.11.0,<3
 PasteDeploy==1.5.0
 routes==1.12.3
 SQLAlchemy>=0.7.8,<0.7.99

commit 64bd0c2699f550c4fd6acb48ba5b73ab289be315

Store created nova client in OpenStackClients.
All created clients are stored in the instance of OpenStackClients
which is associated with a stack - this prevents clients from
being repeatedly created throughout the request.

Commit f3fc5c5 introduced a regression which stopped storing the
nova client. This change addresses that regression.

Change-Id: I60088b60f623aac11e473d185f2924d4f4bd1cfe

heat/engine/clients.py
@@ -98,6 +98,8 @@ class OpenStackClients(object):
             management_url = self.url_for(service_type=service_type)
             client.client.auth_token = con.auth_token
             client.client.management_url = management_url
+
+        self._nova[service_type] = client
         return client
 
     def swift(self):