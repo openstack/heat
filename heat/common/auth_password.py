bd8b68e141f2238bf8555a19602335cc03e6a07e
Ben Nemec <openstack@nemebean.com>
2013-08-11 18:15:17 -0500

    Enable multi-cloud standalone mode
    Adds a multi_cloud configuration parameter for auth_password that
    tells a standalone Heat installation to look for its auth_url in
    the request headers instead of the configuration file.  Also adds
    an allowed_auth_uris configuration option to specify which
    auth_urls are allowed as targets.
    
    bp heat-multicloud
    
    Change-Id: Ic0929586ac3b57c7f9f3335e0dbd5e14e6b56067

--- heat/common/auth_password.py
+++ heat/common/auth_password.py
@@ -18,6 +18,7 @@
 from keystoneclient.v2_0 import client as keystone_client
 from keystoneclient import exceptions as keystone_exceptions
 from oslo.config import cfg
+from webob.exc import HTTPBadRequest
 from webob.exc import HTTPUnauthorized
 
 from heat.openstack.common import importutils
@@ -34,12 +35,15 @@ class KeystonePasswordAuthProtocol(object):
     def __init__(self, app, conf):
         self.app = app
         self.conf = conf
-        if 'auth_uri' in self.conf:
-            auth_url = self.conf['auth_uri']
-        else:
-            # Import auth_token to have keystone_authtoken settings setup.
-            importutils.import_module('keystoneclient.middleware.auth_token')
-            auth_url = cfg.CONF.keystone_authtoken['auth_uri']
+        auth_url = None
+        if not cfg.CONF.auth_password.multi_cloud:
+            if 'auth_uri' in self.conf:
+                auth_url = self.conf['auth_uri']
+            else:
+                # Import auth_token to have keystone_authtoken settings setup.
+                importutils.import_module(
+                    'keystoneclient.middleware.auth_token')
+                auth_url = cfg.CONF.keystone_authtoken['auth_uri']
         self.auth_url = auth_url
 
     def __call__(self, env, start_response):
@@ -48,28 +52,34 @@ class KeystonePasswordAuthProtocol(object):
         password = env.get('HTTP_X_AUTH_KEY')
         # Determine tenant id from path.
         tenant = env.get('PATH_INFO').split('/')[1]
+        auth_url = self.auth_url
+        if cfg.CONF.auth_password.multi_cloud:
+            auth_url = env.get('HTTP_X_AUTH_URL')
+            error = self._validate_auth_url(env, start_response, auth_url)
+            if error:
+                return error
         if not tenant:
-            return self._reject_request(env, start_response)
+            return self._reject_request(env, start_response, auth_url)
         try:
             client = keystone_client.Client(
                 username=username, password=password, tenant_id=tenant,
-                auth_url=self.auth_url)
+                auth_url=auth_url)
         except (keystone_exceptions.Unauthorized,
                 keystone_exceptions.Forbidden,
                 keystone_exceptions.NotFound,
                 keystone_exceptions.AuthorizationFailure):
-            return self._reject_request(env, start_response)
+            return self._reject_request(env, start_response, auth_url)
         env['keystone.token_info'] = client.auth_ref
-        env.update(self._build_user_headers(client.auth_ref))
+        env.update(self._build_user_headers(client.auth_ref, auth_url))
         return self.app(env, start_response)
 
-    def _reject_request(self, env, start_response):
+    def _reject_request(self, env, start_response, auth_url):
         """Redirect client to auth server."""
-        headers = [('WWW-Authenticate', 'Keystone uri=\'%s\'' % self.auth_url)]
+        headers = [('WWW-Authenticate', 'Keystone uri=\'%s\'' % auth_url)]
         resp = HTTPUnauthorized('Authentication required', headers)
         return resp(env, start_response)
 
-    def _build_user_headers(self, token_info):
+    def _build_user_headers(self, token_info, auth_url):
         """Build headers that represent authenticated user from auth token."""
         tenant_id = token_info['token']['tenant']['id']
         tenant_name = token_info['token']['tenant']['name']
@@ -89,7 +99,7 @@ class KeystonePasswordAuthProtocol(object):
             'HTTP_X_ROLES': roles,
             'HTTP_X_SERVICE_CATALOG': service_catalog,
             'HTTP_X_AUTH_TOKEN': auth_token,
-            'HTTP_X_AUTH_URL': self.auth_url,
+            'HTTP_X_AUTH_URL': auth_url,
             # DEPRECATED
             'HTTP_X_USER': user_name,
             'HTTP_X_TENANT_ID': tenant_id,
@@ -99,6 +109,19 @@ class KeystonePasswordAuthProtocol(object):
         }
 
         return headers
+
+    def _validate_auth_url(self, env, start_response, auth_url):
+        """Validate auth_url to ensure it can be used."""
+        if not auth_url:
+            resp = HTTPBadRequest(_('Request missing required header '
+                                    'X-Auth-Url'))
+            return resp(env, start_response)
+        allowed = cfg.CONF.auth_password.allowed_auth_uris
+        if allowed and not auth_url in allowed:
+            resp = HTTPUnauthorized(_('Header X-Auth-Url "%s" not allowed')
+                                    % auth_url)
+            return resp(env, start_response)
+        return None
 
 
 def filter_factory(global_conf, **local_conf):
