a9e7278d00d95a5376a895069624517429c7671b
Zane Bitter <zbitter@redhat.com>
2013-09-04 10:44:39 +0200

    Update nested stacks in parallel
    This parallelises updates for AWS::CloudFormation::Stack resources, but not
    for autoscaling groups. Other types of nested stacks (the old LoadBalancer
    and DBInstance resources, and provider templates) have not yet implemented
    stack updates.
    
    Change-Id: I86e1d5289487b34712c248bc5e68e62cc4651ec6

--- heat/engine/stack_resource.py
+++ heat/engine/stack_resource.py
@@ -136,12 +136,23 @@ class StackResource(resource.Resource):
             self.attributes = None
             self._outputs_to_attribs(child_template)
 
-        nested_stack.update(stack)
+        updater = scheduler.TaskRunner(nested_stack.update_task, stack)
+        updater.start()
+        return updater
 
+    def check_update_complete(self, updater):
+        if updater is None:
+            return True
+
+        if not updater.step():
+            return False
+
+        nested_stack = self.nested()
         if nested_stack.state != (nested_stack.UPDATE,
                                   nested_stack.COMPLETE):
             raise exception.Error("Nested stack update failed: %s" %
                                   nested_stack.status_reason)
+        return True
 
     def delete_nested(self):
         '''
