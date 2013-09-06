350f48f505ed4dea8af5aed0d735b7d548411ee8
Clint Byrum <clint@fewbar.com>
2013-09-06 10:34:53 -0700

    Add DB API to get the count of stacks per tenant
    This method will be used to limit the number of stacks any one tenant
    can create.
    
    Change-Id: Ic990bf84fc1aa5592504f7c423fbce953615a4a1

--- heat/tests/test_sqlalchemy_api.py
+++ heat/tests/test_sqlalchemy_api.py
@@ -203,6 +203,20 @@ class SqlAlchemyTest(HeatTestCase):
         st_db = db_api.stack_get_all_by_tenant(self.ctx)
         self.assertEqual(0, len(st_db))
 
+    def test_stack_count_all_by_tenant(self):
+        stacks = [self._setup_test_stack('stack', x)[1] for x in UUIDs]
+
+        st_db = db_api.stack_count_all_by_tenant(self.ctx)
+        self.assertEqual(2, st_db)
+
+        stacks[0].delete()
+        st_db = db_api.stack_count_all_by_tenant(self.ctx)
+        self.assertEqual(1, st_db)
+
+        stacks[1].delete()
+        st_db = db_api.stack_count_all_by_tenant(self.ctx)
+        self.assertEqual(0, st_db)
+
     def test_event_get_all_by_stack(self):
         stack = self._setup_test_stack('stack', UUID1)[1]
 
