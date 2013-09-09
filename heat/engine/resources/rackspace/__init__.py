3991797483c6f00576c5448a21fd8e0c78dd8612
Randall Burt <randall.burt@rackspace.com>
2013-09-09 18:02:49 -0500

    Move Rackspace resources into contrib
    Using plain discover to run tests in contrib until
    I can figure out how to make that work via testr.
    
    Closes bug: 1220798
    
    Change-Id: I5669b90152efabfacbc4b786a67cd1f4b139521a

--- heat/engine/resources/rackspace/__init__.py
+++
@@ -1,4 +0,0 @@
-# pyrax client library is required to work with Rackspace cloud resources,
-# such as rackspace cloud database, rackspace cloud servers etc...
-# If pyrax client library is not installed, then heat-engine would not register
-# the rackspace cloud resource providers.
