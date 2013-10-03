e99941380006506b5accdef00cb589dfc7d37bad
Steve Baker <sbaker@redhat.com>
2013-10-04 09:28:13 +1300

    Handle NetworkNotFoundClient and PortNotFoundClient
    neutronclient has changed is behavior (recently?) in that it now
    raises PortNotFoundClient for missing ports and NetworkNotFoundClient
    for missing nets. These exceptions extend NeutronClientException but
    do not set the status_code to 404 so the existing resource 404
    checking has stopped working.
    
    This change handles both 404 exceptions and the new exceptions
    so that it continues to work with different neutron releases.
    
    Change-Id: Id06e04d4b98e62b8062b119e959634fd61c8beb5
    Closes-Bug: #1234924

--- heat/engine/resources/neutron/port.py
+++ heat/engine/resources/neutron/port.py
@@ -19,7 +19,7 @@ from heat.engine.resources.neutron import neutron
 from heat.engine import scheduler
 
 if clients.neutronclient is not None:
-    from neutronclient.common.exceptions import NeutronClientException
+    import neutronclient.common.exceptions as neutron_exp
 
 logger = logging.getLogger(__name__)
 
@@ -99,11 +99,16 @@ class Port(neutron.NeutronResource):
         client = self.neutron()
         try:
             client.delete_port(self.resource_id)
-        except NeutronClientException as ex:
-            if ex.status_code != 404:
-                raise ex
+        except neutron_exp.NeutronClientException as ex:
+            self._handle_not_found_exception(ex)
         else:
             return scheduler.TaskRunner(self._confirm_delete)()
+
+    def _handle_not_found_exception(self, ex):
+        # raise any exception which is not for a not found port
+        if not (ex.status_code == 404 or
+                isinstance(ex, neutron_exp.PortNotFoundClient)):
+            raise ex
 
 
 def resource_mapping():
