50dc14af4f78f0d4050f82545aae6910a92aa8cf
Steven Hardy <shardy@redhat.com>
2013-10-18 17:29:31 +0100

    Use "python -m coverage" instead of coverage cli
    As requested by the Debian package maintainer, this makes the
    coverage report work more easily on Debian/Ubuntu
    
    Change-Id: Ibed9e164c89543b679393ee18ccdb4a15bdea6d7
    Closes-Bug: #1241330

--- run_tests.sh
+++ run_tests.sh
@@ -117,7 +117,7 @@ fi
 if [ "$coverage" == 1 ]; then
     echo "Generating coverage report in ./cover"
     python setup.py testr --coverage --slowest
-    coverage report -m
+    python -m coverage report --show-missing
 fi
 
 exit $result
