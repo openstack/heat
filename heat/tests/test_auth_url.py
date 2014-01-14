1e71566169d9ffb34ed4e1bdf3f264cbdbb567cb
Anderson Mesquita <andersonvom@gmail.com>
2014-01-14 10:23:08 -0600

    Assign X-Auth-Url header in a separate middleware
    authtoken middleware's current purpose is to set X-Auth-Url header to a
    value read from heat.conf.
    
    Since this value is not really related to anything the keystone
    middleware does and is instead read from a config file, it makes sense
    to move it from the authtoken middleware into its own middleware
    
    This change is the first step to grouping all X-Auth-Url related logic
    into one single middleware as opposed to have it scattered (or possibly
    repeated) in auth_token and auth_password. For example, auth_password
    also has some logic around it related to multi-cloud, which can be
    extracted and moved to auth_url middleware in a later patch, so that all
    handling of X-Auth-Url occurs in one place.
    
    Also, by extracting the X-Auth-Url logic, it allows cloud
    providers to remove auth_token or auth_password without side-effects.
    
    Closes-Bug: #1259364
    Change-Id: Ieb251c18aa091391a28a90c495b61cf41436f8b9

---
+++ heat/tests/test_auth_url.py
@@ -0,0 +1,63 @@
+# Copyright 2013 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mock
+import webob
+
+from heat.common import auth_url
+from heat.tests.common import HeatTestCase
+
+
+class FakeApp(object):
+    """This represents a WSGI app protected by our auth middleware."""
+
+    def __call__(self, environ, start_response):
+        """Assert that headers are correctly set up when finally called."""
+        resp = webob.Response()
+        resp.body = 'SUCCESS'
+        return resp(environ, start_response)
+
+
+class AuthUrlFilterTest(HeatTestCase):
+
+    def setUp(self):
+        super(AuthUrlFilterTest, self).setUp()
+        self.app = FakeApp()
+        self.config = {'auth_uri': 'foobar'}
+
+    @mock.patch.object(auth_url.cfg, 'CONF')
+    def test_adds_default_auth_url_from_keystone_authtoken(self, mock_cfg):
+        self.config = {}
+        mock_cfg.keystone_authtoken.auth_uri = 'foobar'
+        mock_cfg.auth_password.multi_cloud = False
+        self.middleware = auth_url.AuthUrlFilter(self.app, self.config)
+        req = webob.Request.blank('/tenant_id/')
+        self.middleware(req)
+        self.assertIn('X-Auth-Url', req.headers)
+        self.assertEqual('foobar', req.headers['X-Auth-Url'])
+
+    def test_overwrites_auth_url_from_headers_with_local_config(self):
+        self.middleware = auth_url.AuthUrlFilter(self.app, self.config)
+        req = webob.Request.blank('/tenant_id/')
+        req.headers['X-Auth-Url'] = 'should_be_overwritten'
+        self.middleware(req)
+        self.assertEqual('foobar', req.headers['X-Auth-Url'])
+
+    def test_reads_auth_url_from_local_config(self):
+        self.middleware = auth_url.AuthUrlFilter(self.app, self.config)
+        req = webob.Request.blank('/tenant_id/')
+        self.middleware(req)
+        self.assertIn('X-Auth-Url', req.headers)
+        self.assertEqual('foobar', req.headers['X-Auth-Url'])
