7c6bf662c69db535ad5992a3741acb79b059a87f
Steve Baker <sbaker@redhat.com>
2014-02-20 09:54:28 +1300

    Refactor SupportStatus into its own module
    This will allow a properties to also use SupportStatus
    
    Change-Id: I66783eaefcb7c773120c0f1e5cb7eeacbd483cf7

---
+++ heat/engine/support.py
@@ -0,0 +1,38 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+SUPPORT_STATUSES = (UNKNOWN, SUPPORTED, PROTOTYPE, DEPRECATED,
+                    UNSUPPORTED) = ('UNKNOWN', 'SUPPORTED', 'PROTOTYPE',
+                                    'DEPRECATED', 'UNSUPPORTED')
+
+
+class SupportStatus(object):
+
+    def __init__(self, status=SUPPORTED, message=None, version=None):
+        if status in SUPPORT_STATUSES:
+            self.status = status
+            self.message = message
+            self.version = version
+        else:
+            self.status = UNKNOWN
+            self.message = _("Specified status is invalid, defaulting to"
+                             " %s") % UNKNOWN
+
+            self.version = None
+
+    def to_dict(self):
+            return {'status': self.status,
+                    'message': self.message,
+                    'version': self.version}
