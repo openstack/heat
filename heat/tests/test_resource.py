7081d0f5ac9c9aba5a957208f481e9e60a5dcb1a
Liang Chen <cbjchen@cn.ibm.com>
2014-02-22 18:18:03 +0800

    Enable lazy translation in unit tests
    Change-Id: Ia2aaaaae34f0f234b2493e7c724d7b874035bcfb

--- heat/tests/test_resource.py
+++ heat/tests/test_resource.py
@@ -16,6 +16,7 @@ import json
 import uuid
 
 import mock
+import six
 
 from heat.common import exception
 import heat.db.api as db_api
@@ -522,7 +523,7 @@ class ResourceTest(HeatTestCase):
         updater = scheduler.TaskRunner(res.update, utmpl)
         ex = self.assertRaises(resource.UpdateReplace, updater)
         self.assertEqual('The Resource test_resource requires replacement.',
-                         str(ex))
+                         six.text_type(ex))
         self.m.VerifyAll()
 
     def test_update_replace_without_resource_name(self):
@@ -544,7 +545,7 @@ class ResourceTest(HeatTestCase):
         updater = scheduler.TaskRunner(res.update, utmpl)
         ex = self.assertRaises(resource.UpdateReplace, updater)
         self.assertEqual('The Resource Unknown requires replacement.',
-                         str(ex))
+                         six.text_type(ex))
         self.m.VerifyAll()
 
     def test_update_fail_missing_req_prop(self):
