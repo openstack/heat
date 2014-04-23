60f07fbdacc06189eec4c49509d8e8f9d9720d7f
Dimitri Mazmanov <dimitri.mazmanov@ericsson.com>
2014-04-23 10:58:36 +0200

    Add a Nova Flavor resource.
    This will allow using more fine-grained specification of computational
    resources (RAM, CPU, disk, etc.).
    
    A nova flavor resource has the following format:
    
      heat_template_version: 2013-05-23
      description:  Heat Flavor creation example
        resources:
          test_flavor:
            type: OS::Nova::Flavor
            properties:
              ram: 1024
              vcpus: 1
              disk: 20
              swap: 2
    
    Change-Id: I79812f0ef0d0dc616ccb3361e9b5864faa877df1
    Implements: blueprint dynamic-flavors

---
+++ contrib/nova_flavor/nova_flavor/tests/test_nova_flavor.py
@@ -0,0 +1,88 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from heat.engine import parser
+from heat.engine import template
+from heat.engine import resource
+from heat.tests.common import HeatTestCase
+from heat.tests import utils
+from novaclient.exceptions import NotFound
+
+from ..resources.nova_flavor import resource_mapping  # noqa
+from ..resources.nova_flavor import NovaFlavor  # noqa
+
+flavor_template = {
+    'heat_template_version': '2013-05-23',
+    'resources': {
+        'my_flavor': {
+            'type': 'OS::Nova::Flavor',
+            'properties': {
+                'ram': 1024,
+                'vcpus': 2,
+                'disk': 20,
+                'swap': 2,
+                'rxtx_factor': 1.0,
+                'ephemeral': 0,
+            }
+        }
+    }
+}
+
+
+class NovaFlavorTest(HeatTestCase):
+    def setUp(self):
+        super(NovaFlavorTest, self).setUp()
+
+        self.ctx = utils.dummy_context()
+
+        # For unit testing purpose. Register resource provider
+        # explicitly.
+        resource._register_class("OS::Nova::Flavor", NovaFlavor)
+
+        self.stack = parser.Stack(
+            self.ctx, 'nova_flavor_test_stack',
+            template.Template(flavor_template)
+        )
+
+        self.my_flavor = self.stack['my_flavor']
+        nova = mock.MagicMock()
+        self.novaclient = mock.MagicMock()
+        self.my_flavor.nova = nova
+        nova.return_value = self.novaclient
+        self.flavors = self.novaclient.flavors
+
+    def test_resource_mapping(self):
+        mapping = resource_mapping()
+        self.assertEqual(1, len(mapping))
+        self.assertEqual(NovaFlavor, mapping['OS::Nova::Flavor'])
+        self.assertIsInstance(self.my_flavor, NovaFlavor)
+
+    def test_flavor_handle_create(self):
+        value = mock.MagicMock()
+        flavor_id = '927202df-1afb-497f-8368-9c2d2f26e5db'
+        value.id = flavor_id
+        self.flavors.create.return_value = value
+        self.my_flavor.handle_create()
+        self.assertEqual(flavor_id, self.my_flavor.resource_id)
+
+    def test_flavor_handle_delete(self):
+        self.resource_id = None
+        self.assertIsNone(self.my_flavor.handle_delete())
+        flavor_id = '927202df-1afb-497f-8368-9c2d2f26e5db'
+        self.my_flavor.resource_id = flavor_id
+        self.flavors.delete.return_value = None
+        self.assertIsNone(self.my_flavor.handle_delete())
+        self.flavors.delete.side_effect = NotFound(404, "Not found")
+        self.assertIsNone(self.my_flavor.handle_delete())
