527a5f657e3736ad210c2418fa09ba8aeab5456a
Thomas Spatzier <thomas.spatzier@de.ibm.com>
2014-06-10 17:58:32 +0200

    Add OS::Heat::SoftwareComponent resource
    This patch adds a SoftwareComponent resource for defining
    configurations for a software component's different lifecycle
    actions.
    
    The current patch only adds the basic code for the resource and
    initial test code. A subsequent patch in this chain will add more
    validation (beyond basic property schema validation), e.g. to make
    sure that not more than one config is defined for one lifecycle
    action.
    
    Implements: partial-blueprint action-aware-sw-config
    
    Change-Id: Idd9de7edf3bab99f26911a0d018a1d433381deb4

---
+++ heat/engine/resources/software_config/software_component.py
@@ -0,0 +1,142 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from heat.engine import constraints as constr
+from heat.engine import properties
+from heat.engine import resource
+from heat.engine.resources.software_config import software_config as sc
+
+
+class SoftwareComponent(sc.SoftwareConfig):
+    '''
+    A resource for describing and storing a software component.
+
+    This resource is similar to OS::Heat::SoftwareConfig. In contrast to
+    SoftwareConfig which allows for storing only one configuration (e.g. one
+    script), SoftwareComponent allows for storing multiple configurations to
+    address handling of all lifecycle hooks (CREATE, UPDATE, SUSPEND, RESUME,
+    DELETE) for a software component in one place.
+
+    This resource is backed by the persistence layer and the API of the
+    SoftwareConfig resource, and only adds handling for the additional
+    'configs' property and attribute.
+    '''
+
+    PROPERTIES = (
+        CONFIGS, INPUTS, OUTPUTS, OPTIONS,
+    ) = (
+        'configs', 'inputs', 'outputs', 'options'
+    )
+
+    CONFIG_PROPERTIES = (
+        CONFIG_ACTIONS, CONFIG_CONFIG, CONFIG_TOOL,
+    ) = (
+        'actions', 'config', 'tool',
+    )
+
+    ATTRIBUTES = (
+        CONFIGS_ATTR,
+    ) = (
+        'configs',
+    )
+
+    # properties schema for one entry in the 'configs' list
+    config_schema = properties.Schema(
+        properties.Schema.MAP,
+        schema={
+            CONFIG_ACTIONS: properties.Schema(
+                # Note: This properties schema allows for custom actions to be
+                # specified, which will however require special handling in
+                # in-instance hooks. By default, only the standard actions
+                # stated below will be handled.
+                properties.Schema.LIST,
+                _('Lifecycle actions to which the configuration applies. '
+                  'The string values provided for this property can include '
+                  'the standard resource actions CREATE, DELETE, UPDATE, '
+                  'SUSPEND and RESUME supported by Heat.'),
+                default=[resource.Resource.CREATE, resource.Resource.UPDATE],
+                schema=properties.Schema(properties.Schema.STRING),
+                constraints=[
+                    constr.Length(min=1),
+                ],
+                required=True
+            ),
+            CONFIG_CONFIG: sc.SoftwareConfig.properties_schema[
+                sc.SoftwareConfig.CONFIG
+            ],
+            CONFIG_TOOL: properties.Schema(
+                properties.Schema.STRING,
+                _('The configuration tool used to actually apply the '
+                  'configuration on a server. This string property has '
+                  'to be understood by in-instance tools running inside '
+                  'deployed servers.'),
+                required=True
+            )
+        }
+    )
+
+    properties_schema = {
+        CONFIGS: properties.Schema(
+            properties.Schema.LIST,
+            _('The list of configurations for the different lifecycle actions '
+              'of the represented software component.'),
+            schema=config_schema,
+            constraints=[constr.Length(min=1)],
+            required=True
+        ),
+        INPUTS: sc.SoftwareConfig.properties_schema[
+            sc.SoftwareConfig.INPUTS],
+        OUTPUTS: sc.SoftwareConfig.properties_schema[
+            sc.SoftwareConfig.OUTPUTS],
+        OPTIONS: sc.SoftwareConfig.properties_schema[
+            sc.SoftwareConfig.OPTIONS],
+    }
+
+    def handle_create(self):
+        props = dict(self.properties)
+        props[self.NAME] = self.physical_resource_name()
+        # use config property of SoftwareConfig to store configs list
+        configs = self.properties.get(self.CONFIGS)
+        props[self.CONFIG] = {self.CONFIGS: configs}
+        # set 'group' to enable component processing by in-instance hook
+        props[self.GROUP] = 'component'
+
+        sc = self.heat().software_configs.create(**props)
+        self.resource_id_set(sc.id)
+
+    def _resolve_attribute(self, name):
+        '''
+        Retrieve attributes of the SoftwareComponent resource.
+
+        'configs' returns the list of configurations for the software
+        component's lifecycle actions. If the attribute does not exist,
+        an empty list is returned.
+        '''
+        if name == self.CONFIGS_ATTR and self.resource_id:
+            try:
+                config = self.heat().software_configs.get(self.resource_id).\
+                    config
+                # configs list is stored in 'config' property of parent class
+                # (see handle_create)
+                return config.get(self.CONFIGS)
+            except Exception as ex:
+                if self.client_plugin().is_not_found(ex):
+                    return None
+
+
+def resource_mapping():
+    return {
+        'OS::Heat::SoftwareComponent': SoftwareComponent,
+    }
---
+++ heat/tests/test_software_component.py
@@ -0,0 +1,249 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+import six
+
+from heat.common import exception
+from heat.common import template_format
+from heat.engine.resources.software_config import software_component as sc
+from heat.engine import stack
+from heat.engine import template
+from heat.tests.common import HeatTestCase
+from heat.tests import utils
+from heatclient.exc import HTTPNotFound
+
+
+class SoftwareComponentTest(HeatTestCase):
+
+    def setUp(self):
+        super(SoftwareComponentTest, self).setUp()
+        self.ctx = utils.dummy_context()
+
+        tpl = '''
+        heat_template_version: 2013-05-23
+        resources:
+          mysql_component:
+            type: OS::Heat::SoftwareComponent
+            properties:
+              configs:
+                - actions: [CREATE]
+                  config: |
+                    #!/bin/bash
+                    echo "Create MySQL"
+                  tool: script
+                - actions: [UPDATE]
+                  config: |
+                    #!/bin/bash
+                    echo "Update MySQL"
+                  tool: script
+              inputs:
+                - name: mysql_port
+              outputs:
+                - name: root_password
+        '''
+
+        self.template = template_format.parse(tpl)
+        self.stack = stack.Stack(
+            self.ctx, 'software_component_test_stack',
+            template.Template(self.template))
+        self.component = self.stack['mysql_component']
+        heat = mock.MagicMock()
+        self.heatclient = mock.MagicMock()
+        self.component.heat = heat
+        heat.return_value = self.heatclient
+        self.software_configs = self.heatclient.software_configs
+
+    def test_resource_mapping(self):
+        mapping = sc.resource_mapping()
+        self.assertEqual(1, len(mapping))
+        self.assertEqual(sc.SoftwareComponent,
+                         mapping['OS::Heat::SoftwareComponent'])
+        self.assertIsInstance(self.component, sc.SoftwareComponent)
+
+    def test_handle_create(self):
+        value = mock.MagicMock()
+        config_id = 'c8a19429-7fde-47ea-a42f-40045488226c'
+        value.id = config_id
+        self.software_configs.create.return_value = value
+        self.component.handle_create()
+        self.assertEqual(config_id, self.component.resource_id)
+
+    def test_handle_delete(self):
+        self.resource_id = None
+        self.assertIsNone(self.component.handle_delete())
+        config_id = 'c8a19429-7fde-47ea-a42f-40045488226c'
+        self.component.resource_id = config_id
+        self.software_configs.delete.return_value = None
+        self.assertIsNone(self.component.handle_delete())
+        self.software_configs.delete.side_effect = HTTPNotFound()
+        self.assertIsNone(self.component.handle_delete())
+
+    def test_resolve_attribute(self):
+        self.assertIsNone(self.component._resolve_attribute('others'))
+        self.component.resource_id = None
+        self.assertIsNone(self.component._resolve_attribute('configs'))
+        self.component.resource_id = 'c8a19429-7fde-47ea-a42f-40045488226c'
+        value = mock.MagicMock()
+        configs = self.\
+            template['resources']['mysql_component']['properties']['configs']
+        # configs list is stored in 'config' property of SoftwareConfig
+        value.config = {'configs': configs}
+        self.software_configs.get.return_value = value
+        self.assertEqual(configs, self.component._resolve_attribute('configs'))
+        self.software_configs.get.side_effect = HTTPNotFound()
+        self.assertIsNone(self.component._resolve_attribute('configs'))
+
+
+class SoftwareComponentValidationTest(HeatTestCase):
+
+    scenarios = [
+        (
+            'component_full',
+            dict(snippet='''
+                 component:
+                   type: OS::Heat::SoftwareComponent
+                   properties:
+                     configs:
+                       - actions: [CREATE]
+                         config: |
+                           #!/bin/bash
+                           echo CREATE $foo
+                         tool: script
+                     inputs:
+                       - name: foo
+                     outputs:
+                       - name: bar
+                     options:
+                       opt1: blah
+                 ''',
+                 err=None,
+                 err_msg=None)
+        ),
+        (
+            'no_input_output_options',
+            dict(snippet='''
+                 component:
+                   type: OS::Heat::SoftwareComponent
+                   properties:
+                     configs:
+                       - actions: [CREATE]
+                         config: |
+                           #!/bin/bash
+                           echo CREATE $foo
+                         tool: script
+                 ''',
+                 err=None,
+                 err_msg=None)
+        ),
+        (
+            'wrong_property_config',
+            dict(snippet='''
+                 component:
+                   type: OS::Heat::SoftwareComponent
+                   properties:
+                     config: #!/bin/bash
+                     configs:
+                       - actions: [CREATE]
+                         config: |
+                           #!/bin/bash
+                           echo CREATE $foo
+                         tool: script
+                 ''',
+                 err=exception.StackValidationFailed,
+                 err_msg='Unknown Property config')
+        ),
+        (
+            'missing_configs',
+            dict(snippet='''
+                 component:
+                   type: OS::Heat::SoftwareComponent
+                   properties:
+                     inputs:
+                       - name: foo
+                 ''',
+                 err=exception.StackValidationFailed,
+                 err_msg='Property configs not assigned')
+        ),
+        # do not test until bug #1350840
+#         (
+#             'empty_configs',
+#             dict(snippet='''
+#                  component:
+#                    type: OS::Heat::SoftwareComponent
+#                    properties:
+#                      configs:
+#                  ''',
+#                  err=exception.StackValidationFailed,
+#                  err_msg='configs length (0) is out of range '
+#                                     '(min: 1, max: None)')
+#         ),
+        (
+            'invalid_configs',
+            dict(snippet='''
+                 component:
+                   type: OS::Heat::SoftwareComponent
+                   properties:
+                     configs:
+                       actions: [CREATE]
+                       config: #!/bin/bash
+                       tool: script
+                 ''',
+                 err=exception.StackValidationFailed,
+                 err_msg='is not a list')
+        ),
+        (
+            'config_empty_actions',
+            dict(snippet='''
+                 component:
+                   type: OS::Heat::SoftwareComponent
+                   properties:
+                     configs:
+                       - actions: []
+                         config: #!/bin/bash
+                         tool: script
+                 ''',
+                 err=exception.StackValidationFailed,
+                 err_msg='actions length (0) is out of range '
+                         '(min: 1, max: None)')
+        ),
+    ]
+
+    def setUp(self):
+        super(SoftwareComponentValidationTest, self).setUp()
+        self.ctx = utils.dummy_context()
+
+        tpl = '''
+        heat_template_version: 2013-05-23
+        resources:
+          %s
+        ''' % self.snippet
+
+        self.template = template_format.parse(tpl)
+        self.stack = stack.Stack(
+            self.ctx, 'software_component_test_stack',
+            template.Template(self.template))
+        self.component = self.stack['component']
+        heat = mock.MagicMock()
+        self.heatclient = mock.MagicMock()
+        self.component.heat = heat
+        heat.return_value = self.heatclient
+        self.software_configs = self.heatclient.software_configs
+
+    def test_properties_schema(self):
+        if self.err:
+            err = self.assertRaises(self.err, self.stack.validate)
+            if self.err_msg:
+                self.assertIn(self.err_msg, six.text_type(err))
+        else:
+            self.assertIsNone(self.stack.validate())
