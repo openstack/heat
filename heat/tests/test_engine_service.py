openstack/heat

commit dde5a81e8060b373982f165856071a80be1d40bb

List stacks using Stack.load_all()
Abstract the code to list stacks in the database out of service.py and into
the Stack class itself.

Change-Id: If27a184ffc87067f708868f91aaf380753f2de3b

heat/engine/service.py
@@ -398,15 +398,12 @@ class EngineService(service.Service):
             to show all
         """
         if stack_identity is not None:
-            stacks = [self._get_stack(cnxt, stack_identity, show_deleted=True)]
+            db_stack = self._get_stack(cnxt, stack_identity, show_deleted=True)
+            stacks = [parser.Stack.load(cnxt, stack=db_stack)]
         else:
-            stacks = db_api.stack_get_all(cnxt) or []
+            stacks = parser.Stack.load_all(cnxt)
 
-        def format_stack_detail(s):
-            stack = parser.Stack.load(cnxt, stack=s)
-            return api.format_stack(stack)
-
-        return [format_stack_detail(s) for s in stacks]
+        return [api.format_stack(stack) for stack in stacks]
 
     def get_revision(self, cnxt):
         return cfg.CONF.revision['heat_revision']
@@ -430,22 +427,10 @@ class EngineService(service.Service):
         :param show_deleted: if true, show soft-deleted stacks
         :returns: a list of formatted stacks
         """
-        def format_stack_details(stacks):
-            for s in stacks:
-                try:
-                    stack = parser.Stack.load(cnxt, stack=s,
-                                              resolve_data=False)
-                except exception.NotFound:
-                    # The stack may have been deleted between listing
-                    # and formatting
-                    pass
-                else:
-                    yield api.format_stack(stack)
-
-        stacks = db_api.stack_get_all(cnxt, limit, sort_keys, marker,
-                                      sort_dir, filters, tenant_safe,
-                                      show_deleted) or []
-        return list(format_stack_details(stacks))
+        stacks = parser.Stack.load_all(cnxt, limit, sort_keys, marker,
+                                       sort_dir, filters, tenant_safe,
+                                       show_deleted, resolve_data=False)
+        return [api.format_stack(stack) for stack in stacks]
 
     @request_context
     def count_stacks(self, cnxt, filters=None, tenant_safe=True):

heat/tests/test_engine_service.py
@@ -1667,8 +1667,8 @@ class StackServiceTest(HeatTestCase):
 
     @stack_context('service_list_all_test_stack')
     def test_stack_list_all(self):
-        self.m.StubOutWithMock(parser.Stack, 'load')
-        parser.Stack.load(self.ctx, stack=mox.IgnoreArg(), resolve_data=False)\
+        self.m.StubOutWithMock(parser.Stack, '_from_db')
+        parser.Stack._from_db(self.ctx, mox.IgnoreArg(), resolve_data=False)\
             .AndReturn(self.stack)
 
         self.m.ReplayAll()

heat/engine/parser.py
@@ -187,8 +187,8 @@ class Stack(collections.Mapping):
         return deps
 
     @classmethod
-    def load(cls, context, stack_id=None, stack=None, resolve_data=True,
-             parent_resource=None, show_deleted=True):
+    def load(cls, context, stack_id=None, stack=None, parent_resource=None,
+             show_deleted=True):
         '''Retrieve a Stack from the database.'''
         if stack is None:
             stack = db_api.stack_get(context, stack_id,
@@ -198,8 +198,17 @@ class Stack(collections.Mapping):
             message = _('No stack exists with id "%s"') % str(stack_id)
             raise exception.NotFound(message)
 
-        return cls._from_db(context, stack, parent_resource=parent_resource,
-                            resolve_data=resolve_data)
+        return cls._from_db(context, stack, parent_resource=parent_resource)
+
+    @classmethod
+    def load_all(cls, context, limit=None, marker=None, sort_keys=None,
+                 sort_dir=None, filters=None, tenant_safe=True,
+                 show_deleted=False, resolve_data=True):
+        stacks = db_api.stack_get_all(context, limit, sort_keys, marker,
+                                      sort_dir, filters, tenant_safe,
+                                      show_deleted) or []
+        for stack in stacks:
+            yield cls._from_db(context, stack, resolve_data=resolve_data)
 
     @classmethod
     def _from_db(cls, context, stack, parent_resource=None, resolve_data=True):

commit b68e9dc62915a474783233a18888f8934f9d72ed

Fix pagination of stack-list
This was broken by the refactor in dde5a81e8060b373982f165856071a80be1d40bb
because the arguments to the DB call are in a different order to the ones
in the API, presumably for historical reasons.

Change-Id: I7503feb62089b252766a510281e0e4513945ad63
Closes-Bug: #1332611

heat/engine/service.py
@@ -452,7 +452,7 @@ class EngineService(service.Service):
         :param show_deleted: if true, show soft-deleted stacks
         :returns: a list of formatted stacks
         """
-        stacks = parser.Stack.load_all(cnxt, limit, sort_keys, marker,
+        stacks = parser.Stack.load_all(cnxt, limit, marker, sort_keys,
                                        sort_dir, filters, tenant_safe,
                                        show_deleted, resolve_data=False)
         return [api.format_stack(stack) for stack in stacks]

heat/tests/test_engine_service.py
@@ -1705,6 +1705,24 @@ class StackServiceTest(HeatTestCase):
         self.m.VerifyAll()
 
     @mock.patch.object(db_api, 'stack_get_all')
+    def test_stack_list_passes_marker_info(self, mock_stack_get_all):
+        limit = object()
+        marker = object()
+        sort_keys = object()
+        sort_dir = object()
+        self.eng.list_stacks(self.ctx, limit=limit, marker=marker,
+                             sort_keys=sort_keys, sort_dir=sort_dir)
+        mock_stack_get_all.assert_called_once_with(self.ctx,
+                                                   limit,
+                                                   sort_keys,
+                                                   marker,
+                                                   sort_dir,
+                                                   mock.ANY,
+                                                   mock.ANY,
+                                                   mock.ANY,
+                                                   )
+
+    @mock.patch.object(db_api, 'stack_get_all')
     def test_stack_list_passes_filtering_info(self, mock_stack_get_all):
         filters = {'foo': 'bar'}
         self.eng.list_stacks(self.ctx, filters=filters)