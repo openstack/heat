7d1cf3d680cea7dec02bf1dec9ee19abb61cf9eb
Pavlo Shchelokovskyy <pshchelokovskyy@mirantis.com>
2014-07-13 19:53:04 +0300

    Amend docs with `immutable` property attribute.
    Include warning about updates not being supported for a propety
    based on its `immutable` value being True.
    
    Change-Id: I10fe495a2a35429b3d6e254c23ae91732e2e4dd8
    Implements: blueprint implement-aws-updates-not-supported

--- doc/source/ext/resources.py
+++ doc/source/ext/resources.py
@@ -27,7 +27,6 @@ from heat.engine import support
 from heat.openstack.common.gettextutils import _
 
 
-
 global_env = environment.Environment({}, user_env=False)
 
 
@@ -218,6 +217,11 @@ Resources:
         if prop.update_allowed:
             para = nodes.paragraph('',
                                    _('Can be updated without replacement.'))
+            definition.append(para)
+        elif prop.immutable:
+            para = nodes.paragraph('', _('Updates are not supported. '
+                                         'Resource update will fail on any '
+                                         'attempt to update this property.'))
             definition.append(para)
         else:
             para = nodes.paragraph('', _('Updates cause replacement.'))
