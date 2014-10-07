9cb907848f1de44c4037da8980f848f1e2597141
Steve Baker <sbaker@redhat.com>
2014-10-07 13:22:41 +1300

    Configure flavor and image from env
    Create nova flavor and set environment variables
    to set default configuration values for running integration tests.
    
    Change-Id: I47d386159e2c5283459b7550886df25970707238

--- heat_integrationtests/post_test_hook.sh
+++ heat_integrationtests/post_test_hook.sh
@@ -14,6 +14,14 @@
 
 # This script is executed inside post_test_hook function in devstack gate.
 
+# Register the flavor for booting test servers
+source /opt/stack/new/devstack/accrc/admin/admin
+export HEAT_TEST_INSTANCE_TYPE=m1.heat_int
+nova flavor-create $HEAT_TEST_INSTANCE_TYPE 452 512 0 1
+
+export HEAT_TEST_IMAGE_REF=Fedora-x86_64-20-20140618-sda
+export HEAT_TEST_MINIMAL_IMAGE_REF=cirros-0.3.2-x86_64-uec
+
 source /opt/stack/new/devstack/accrc/demo/demo
 cd /opt/stack/new/heat
 sudo -E tox -eintegration
