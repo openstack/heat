72d0313fab470534d2cffb6c7b8a6c311d68740c
ZHU ZHU <zhuzhubj@cn.ibm.com>
2014-11-13 04:39:04 -0600

    Fix incorrect ca argument for heat ceilometer client
    Currently Heat ceilometer client are passing the wrong ca argument
    during client instantiation. It will cause heat fail to connect to ceilometer
    if it's configured with SSL.
    
    https://github.com/openstack/python-ceilometerclient/blob/master/ceilometerclient/v2/client.py#L53
    
    Closes-bug: #1391792
    
    Change-Id: Ib32dfede0ece78bc39e0cab213cb51df8eb62eb6

--- heat/engine/clients/os/ceilometer.py
+++ heat/engine/clients/os/ceilometer.py
@@ -34,7 +34,7 @@ class CeilometerClientPlugin(client_plugin.ClientPlugin):
             'project_id': con.tenant,
             'token': lambda: self.auth_token,
             'endpoint_type': endpoint_type,
-            'ca_file': self._get_client_option('ceilometer', 'ca_file'),
+            'cacert': self._get_client_option('ceilometer', 'ca_file'),
             'cert_file': self._get_client_option('ceilometer', 'cert_file'),
             'key_file': self._get_client_option('ceilometer', 'key_file'),
             'insecure': self._get_client_option('ceilometer', 'insecure')
