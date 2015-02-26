dcefecbfc9df6a9dcaa88f2e99b7a4612077fdca
Anant Patil <anant.patil@hp.com>
2015-02-26 18:08:10 +0530

    Utility for database migration.
    The DB migration often needs to clone a table into a new one as
    databases like sqlite do not support most of the ALTER commands for
    in-place modification of table schema. This utility has method to clone
    a table to new one so that a new table can be created and data migrated
    to it for handling migration. This is fairly generic, therefore new file
    is added.
    
    New generic functions can be added here when needed.
    
    Co-Authored-By: Visnusaran Murugan <visnusaran.murugan@hp.com>
    
    Change-Id: I229907dc2cab618ca5c76a72956ce8c5f49a2127

---
+++ heat/db/sqlalchemy/utils.py
@@ -0,0 +1,59 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# SQLAlchemy helper functions
+
+import sqlalchemy
+
+
+def clone_table(name, parent, meta, newcols=[], ignorecols=[], swapcols={},
+                ignorecons=[]):
+    """
+    helper function that clones parent table schema onto
+    new table.
+
+    :param name: new table name
+    :param parent: parent table to copy schema from
+    :param newcols: names of new columns to be added
+    :param ignorecols: names of columns to be ignored while cloning
+    :param swapcols: alternative column schema
+    :param ignorecons: names of constraints to be ignored
+
+    :return: sqlalchemy.Table instance
+    """
+
+    cols = [c.copy() for c in parent.columns
+            if c.name not in ignorecols
+            if c.name not in swapcols]
+    cols.extend(swapcols.values())
+    cols.extend(newcols)
+    new_table = sqlalchemy.Table(name, meta, *(cols))
+
+    def _is_ignorable(cons):
+        # consider constraints on columns only
+        if hasattr(cons, 'columns'):
+            for col in ignorecols:
+                if col in cons.columns:
+                    return True
+
+        return False
+
+    constraints = [c.copy() for c in parent.constraints
+                   if c.name not in ignorecons
+                   if not _is_ignorable(c)]
+
+    for c in constraints:
+        new_table.append_constraint(c)
+
+    new_table.create()
+    return new_table
