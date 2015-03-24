012107dedc4c5d6a06f02bd0f1d7c49485c7a10f
Tetiana Lashchova <tlashchova@mirantis.com>
2015-03-24 15:15:47 +0200

    Get 'update' paragraph from top-level schema
    Change-Id: I4e3ed92303fa268afd5583cc76e3536b18478294
    Closes-Bug: #1399120

--- heat/tests/test_properties.py
+++ heat/tests/test_properties.py
@@ -19,6 +19,7 @@ from heat.engine.cfn import functions as cfn_funcs
 from heat.engine import constraints
 from heat.engine.hot import parameters as hot_param
 from heat.engine import parameters
+from heat.engine import plugin_manager
 from heat.engine import properties
 from heat.engine import resources
 from heat.engine import support
@@ -580,6 +581,43 @@ class PropertySchemaTest(common.HeatTestCase):
                          schema.default)
         self.assertFalse(schema.required)
         self.assertTrue(schema.allow_conversion)
+
+    def test_no_mismatch_in_update_policy(self):
+        manager = plugin_manager.PluginManager('heat.engine.resources')
+        resource_mapping = plugin_manager.PluginMapping('resource')
+        res_plugin_mappings = resource_mapping.load_all(manager)
+        all_resources = {}
+        for mapping in res_plugin_mappings:
+            name, cls = mapping
+            all_resources[name] = cls
+
+        def check_update_policy(resource_type, prop_key, prop, update=False):
+            if prop.update_allowed:
+                update = True
+            sub_schema = prop.schema
+            if sub_schema:
+                for sub_prop_key, sub_prop in six.iteritems(sub_schema):
+                    if not update:
+                        self.assertEqual(update, sub_prop.update_allowed,
+                                         "Mismatch in update policies: "
+                                         "resource %(res)s, properties "
+                                         "'%(prop)s' and '%(nested_prop)s'." %
+                                         {'res': resource_type,
+                                          'prop': prop_key,
+                                          'nested_prop': sub_prop_key})
+                    if sub_prop_key is '*':
+                        check_update_policy(resource_type, prop_key,
+                                            sub_prop, update)
+                    else:
+                        check_update_policy(resource_type, sub_prop_key,
+                                            sub_prop, update)
+
+        for resource_type, resource_class in six.iteritems(all_resources):
+            props_schemata = properties.schemata(
+                resource_class.properties_schema)
+
+            for prop_key, prop in six.iteritems(props_schemata):
+                check_update_policy(resource_type, prop_key, prop)
 
 
 class PropertyTest(common.HeatTestCase):
