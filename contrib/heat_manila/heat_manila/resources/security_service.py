4e1fff5a9e458047c896788fcdb4a382bcfeaf3e
Tetiana Lashchova <tlashchova@mirantis.com>
2015-04-06 20:21:57 +0300

    Implement OS::Manila::SecurityService resource type
    Change-Id: I4cdda307b2331ce6657950cd46aac1a9270900ea
    Implements: partial blueprint add-manila-resources

---
+++ contrib/heat_manila/heat_manila/resources/security_service.py
@@ -0,0 +1,111 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from heat.common.i18n import _
+from heat.engine import constraints
+from heat.engine import properties
+from heat.engine import resource
+from heat.engine import support
+
+
+class SecurityService(resource.Resource):
+    """
+    A security_service is a set of options that defines a security domain
+    for a particular shared filesystem protocol, such as an
+    Active Directory domain or a Kerberos domain.
+    """
+
+    support_status = support.SupportStatus(version='2015.2')
+
+    PROPERTIES = (
+        NAME, TYPE, DNS_IP, SERVER, DOMAIN, USER,
+        PASSWORD, DESCRIPTION
+    ) = (
+        'name', 'type', 'dns_ip', 'server', 'domain', 'user',
+        'password', 'description'
+    )
+
+    properties_schema = {
+        NAME: properties.Schema(
+            properties.Schema.STRING,
+            _('Security service name.'),
+            update_allowed=True
+        ),
+        TYPE: properties.Schema(
+            properties.Schema.STRING,
+            _('Security service type.'),
+            required=True,
+            constraints=[
+                constraints.AllowedValues(['ldap', 'kerberos',
+                                           'active_directory'])
+            ]
+        ),
+        DNS_IP: properties.Schema(
+            properties.Schema.STRING,
+            _('DNS IP address used inside tenant\'s network.'),
+            update_allowed=True
+        ),
+        SERVER: properties.Schema(
+            properties.Schema.STRING,
+            _('Security service IP address or hostname.'),
+            update_allowed=True
+        ),
+        DOMAIN: properties.Schema(
+            properties.Schema.STRING,
+            _('Security service domain.'),
+            update_allowed=True
+        ),
+        USER: properties.Schema(
+            properties.Schema.STRING,
+            _('Security service user or group used by tenant.'),
+            update_allowed=True
+        ),
+        PASSWORD: properties.Schema(
+            properties.Schema.STRING,
+            _('Password used by user.'),
+            update_allowed=True
+        ),
+        DESCRIPTION: properties.Schema(
+            properties.Schema.STRING,
+            _('Security service description.'),
+            update_allowed=True
+        )
+    }
+
+    default_client_name = 'manila'
+
+    def handle_create(self):
+        args = dict((k, v) for k, v in self.properties.items()
+                    if v is not None)
+        security_service = self.client().security_services.create(**args)
+        self.resource_id_set(security_service.id)
+
+    def handle_update(self, json_snippet=None, tmpl_diff=None, prop_diff=None):
+        if prop_diff:
+            self.client().security_services.update(self.resource_id,
+                                                   **prop_diff)
+
+    def handle_delete(self):
+        if self.resource_id is None:
+            return
+
+        try:
+            self.client().security_services.delete(self.resource_id)
+        except Exception as ex:
+            self.client_plugin().ignore_not_found(ex)
+
+
+def resource_mapping():
+    return {
+        'OS::Manila::SecurityService': SecurityService
+    }
---
+++ contrib/heat_manila/heat_manila/tests/test_security_service.py
@@ -0,0 +1,170 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+import six
+
+from heat.common import exception
+from heat.common import template_format
+from heat.engine import resource
+from heat.engine import scheduler
+from heat.engine import template
+from heat.tests import common
+from heat.tests import utils
+
+from ..resources import security_service  # noqa
+
+stack_template = '''
+heat_template_version: 2013-05-23
+
+resources:
+  security_service:
+    type: OS::Manila::SecurityService
+    properties:
+      name: my_security_service
+      domain: test-domain
+      dns_ip: 1.1.1.1
+      type: ldap
+      server: test-server
+      user: test-user
+      password: test-password
+'''
+
+stack_template_update = '''
+heat_template_version: 2013-05-23
+
+resources:
+  security_service:
+    type: OS::Manila::SecurityService
+    properties:
+      name: fake_security_service
+      domain: fake-domain
+      dns_ip: 1.1.1.1
+      type: ldap
+      server: fake-server
+'''
+
+stack_template_update_replace = '''
+heat_template_version: 2013-05-23
+
+resources:
+  security_service:
+    type: OS::Manila::SecurityService
+    properties:
+      name: my_security_service
+      domain: test-domain
+      dns_ip: 1.1.1.1
+      type: kerberos
+      server: test-server
+      user: test-user
+      password: test-password
+'''
+
+
+class SecurityServiceTest(common.HeatTestCase):
+
+    def setUp(self):
+        super(SecurityServiceTest, self).setUp()
+        utils.setup_dummy_db()
+        self.ctx = utils.dummy_context()
+
+        t = template_format.parse(stack_template)
+        self.stack = utils.parse_stack(t)
+        resource_defns = self.stack.t.resource_definitions(self.stack)
+        self.rsrc_defn = resource_defns['security_service']
+
+        self.client = mock.Mock()
+        self.patchobject(security_service.SecurityService, 'client',
+                         return_value=self.client)
+
+    def _create_resource(self, name, snippet, stack):
+        ss = security_service.SecurityService(name, snippet, stack)
+        value = mock.MagicMock(id='12345')
+        self.client.security_services.create.return_value = value
+        self.client.security_services.get.return_value = value
+        scheduler.TaskRunner(ss.create)()
+        args = self.client.security_services.create.call_args[1]
+        self.assertEqual(self.rsrc_defn['Properties'], args)
+        self.assertEqual('12345', ss.resource_id)
+        return ss
+
+    def test_create(self):
+        ct = self._create_resource('security_service', self.rsrc_defn,
+                                   self.stack)
+        expected_state = (ct.CREATE, ct.COMPLETE)
+        self.assertEqual(expected_state, ct.state)
+
+    def test_create_failed(self):
+        ss = security_service.SecurityService('security_service',
+                                              self.rsrc_defn, self.stack)
+        self.client.security_services.create.side_effect = Exception('error')
+
+        exc = self.assertRaises(exception.ResourceFailure,
+                                scheduler.TaskRunner(ss.create))
+        expected_state = (ss.CREATE, ss.FAILED)
+        self.assertEqual(expected_state, ss.state)
+        self.assertIn('Exception: error', six.text_type(exc))
+
+    def test_resource_mapping(self):
+        mapping = security_service.resource_mapping()
+        self.assertEqual(1, len(mapping))
+        self.assertEqual(security_service.SecurityService,
+                         mapping['OS::Manila::SecurityService'])
+
+    def test_delete(self):
+        ss = self._create_resource('security_service', self.rsrc_defn,
+                                   self.stack)
+        scheduler.TaskRunner(ss.delete)()
+        self.assertEqual((ss.DELETE, ss.COMPLETE), ss.state)
+        self.client.security_services.delete.assert_called_once_with(
+            ss.resource_id)
+
+    def test_delete_not_found(self):
+        ss = self._create_resource('security_service', self.rsrc_defn,
+                                   self.stack)
+        self.client.security_services.delete.side_effect = (
+            self.client.exceptions.NotFound())
+        scheduler.TaskRunner(ss.delete)()
+        self.assertEqual((ss.DELETE, ss.COMPLETE), ss.state)
+        self.client.security_services.delete.assert_called_once_with(
+            ss.resource_id)
+
+    def test_update(self):
+        ss = self._create_resource('security_service', self.rsrc_defn,
+                                   self.stack)
+        t = template_format.parse(stack_template_update)
+        rsrc_defns = template.Template(t).resource_definitions(self.stack)
+        new_ss = rsrc_defns['security_service']
+        scheduler.TaskRunner(ss.update, new_ss)()
+        args = {
+            'domain': 'fake-domain',
+            'password': None,
+            'user': None,
+            'server': 'fake-server',
+            'name': 'fake_security_service'
+        }
+        self.client.security_services.update.assert_called_once_with(
+            '12345', **args)
+        self.assertEqual((ss.UPDATE, ss.COMPLETE), ss.state)
+
+    def test_update_replace(self):
+        ss = self._create_resource('security_service', self.rsrc_defn,
+                                   self.stack)
+        t = template_format.parse(stack_template_update_replace)
+        rsrc_defns = template.Template(t).resource_definitions(self.stack)
+        new_ss = rsrc_defns['security_service']
+        self.assertEqual(0, self.client.security_services.update.call_count)
+        err = self.assertRaises(resource.UpdateReplace,
+                                scheduler.TaskRunner(ss.update, new_ss))
+        msg = 'The Resource security_service requires replacement.'
+        self.assertEqual(msg, six.text_type(err))
