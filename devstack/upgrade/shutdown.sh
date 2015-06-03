334b22929cc5c37ba48d86addca74ea087499afd
Sirushti Murugesan <sirushti.murugesan@hp.com>
2015-06-04 00:29:47 +0530

    In-tree grenade support for Heat
    The following implements in tree support for grenade upgrade testing
    for heat. It includes the expected directory structure to be used for
    the external plugin.
    
    For now, we create a simple OS::Heat::RandomString resource
    and check whether the stack survives the upgrade.
    We should add better tests for upgrades in the future.
    
    Co-Authored-By: Steven Hardy <shardy@redhat.com>
    Co-Authored-By: Sergey Kraynev <skraynev@mirantis.com>
    Co-Authored-By: Emilien Macchi <emilien.macchi@enovance.com>
    
    partial blueprint upgrade-tests
    
    Change-Id: I0847004a8ac023a113cabaee46896abc823a01da

---
+++ devstack/upgrade/shutdown.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+set -o errexit
+
+source $GRENADE_DIR/grenaderc
+source $GRENADE_DIR/functions
+
+# We need base DevStack functions for this
+source $BASE_DEVSTACK_DIR/functions
+source $BASE_DEVSTACK_DIR/stackrc # needed for status directory
+source $BASE_DEVSTACK_DIR/lib/tls
+source $BASE_DEVSTACK_DIR/lib/heat
+
+set -o xtrace
+
+stop_heat
+
+SERVICES_DOWN="heat-api heat-engine heat-api-cfn heat-api-cloudwatch"
+
+# sanity check that services are actually down
+ensure_services_stopped $SERVICES_DOWN
