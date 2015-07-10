8f02c9b5401db5e6c4d478268a8cb843df6a1723
Steven Hardy <shardy@redhat.com>
2015-07-10 09:39:00 +0100

    Add OS::Heat::None resource
    Adds a new resource, designed to enable easy "stubbing out"
    of resources via the environment resource_registry.
    
    It takes any properties, and returns any attribute (as None),
    meaning it can be uses in place of "noop" nested stacks which
    dummy an existing resources by matching parameters/outputs to
    properties/attributes.
    
    Implements: blueprint noop-resource
    Change-Id: Idc485a9fcf7287030dda22f893486279be9e1a68

---
+++ heat/engine/resources/openstack/heat/none_resource.py
@@ -0,0 +1,56 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import six
+import uuid
+
+from heat.engine import properties
+from heat.engine import resource
+from heat.engine import support
+
+
+class NoneResource(resource.Resource):
+    '''
+    A resource which enables easily disabling certain resources via the
+    resource_registry.  It does nothing, but can effectively stub out
+    any other resource because it will accept any properties and return
+    any attribute (as None).  Note this resource always does nothing
+    on update (e.g it is not replaced even if a change to the stubbed
+    resource properties would cause replacement).
+    '''
+
+    support_status = support.SupportStatus(version='5.0.0')
+    properties_schema = {}
+    attributes_schema = {}
+
+    def _needs_update(self, after, before, after_props, before_props,
+                      prev_resource):
+        return False
+
+    def reparse(self):
+        self.properties = properties.Properties(schema={}, data={})
+
+    def handle_create(self):
+        self.resource_id_set(six.text_type(uuid.uuid4()))
+
+    def validate(self):
+        pass
+
+    def FnGetAtt(self, key, *path):
+        return None
+
+
+def resource_mapping():
+    return {
+        'OS::Heat::None': NoneResource,
+    }
