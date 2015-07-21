aded1c768f8ad656c70f4f40cc3c2399a97b31d1
Oleksii Chuprykov <ochuprykov@mirantis.com>
2015-07-21 15:52:39 +0300

    Add test for ceilometer client
    Change-Id: I65afe1d4f33d1e568dd530c6f7350770f4eba576
    Partial-Bug: #1476220

---
+++ heat/tests/clients/test_ceilometer_client.py
@@ -0,0 +1,26 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from heat.tests import common
+from heat.tests import utils
+
+
+class CeilometerClientPluginTests(common.HeatTestCase):
+
+    def test_create(self):
+        context = utils.dummy_context()
+        plugin = context.clients.client_plugin('ceilometer')
+        client = plugin.client()
+        self.assertIsNotNone(client.alarms)
+        self.assertEqual('http://server.test:5000/v2.0',
+                         client.auth_plugin.opts['auth_url'])
