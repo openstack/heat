3f68d5485e45ac665a05f0b13c1986f716a1508d
ricolin <rico.l@inwinstack.com>
2015-07-30 17:48:04 +0800

    move network constraints from neutron to common constraint
    seperate out common network constraints (ip_addr, cidr, and mac_addr)
    
    Change-Id: I617035e379c4e86ac0ce4c77f26ebfca5b3d691a

--- heat/tests/constraints/test_common_constraints.py
+++ heat/tests/constraints/test_common_constraints.py
@@ -15,6 +15,105 @@ from heat.engine.constraint import common_constraints as cc
 from heat.tests import common
 
 
+class TestIPConstraint(common.HeatTestCase):
+
+    def setUp(self):
+        super(TestIPConstraint, self).setUp()
+        self.constraint = cc.IPConstraint()
+
+    def test_validate_ipv4_format(self):
+        validate_format = [
+            '1.1.1.1',
+            '1.0.1.1',
+            '255.255.255.255'
+        ]
+        for ip in validate_format:
+            self.assertTrue(self.constraint.validate(ip, None))
+
+    def test_invalidate_ipv4_format(self):
+        invalidate_format = [
+            '1.1.1.',
+            '1.1.1.256',
+            'invalidate format',
+            '1.a.1.1'
+        ]
+        for ip in invalidate_format:
+            self.assertFalse(self.constraint.validate(ip, None))
+
+    def test_validate_ipv6_format(self):
+        validate_format = [
+            '2002:2002::20c:29ff:fe7d:811a',
+            '::1',
+            '2002::',
+            '2002::1',
+        ]
+        for ip in validate_format:
+            self.assertTrue(self.constraint.validate(ip, None))
+
+    def test_invalidate_ipv6_format(self):
+        invalidate_format = [
+            '2002::2001::1',
+            '2002::g',
+            'invalidate format',
+            '2001::0::',
+            '20c:29ff:fe7d:811a'
+        ]
+        for ip in invalidate_format:
+            self.assertFalse(self.constraint.validate(ip, None))
+
+
+class TestMACConstraint(common.HeatTestCase):
+
+    def setUp(self):
+        super(TestMACConstraint, self).setUp()
+        self.constraint = cc.MACConstraint()
+
+    def test_valid_mac_format(self):
+        validate_format = [
+            '01:23:45:67:89:ab',
+            '01-23-45-67-89-ab',
+            '0123.4567.89ab'
+        ]
+        for mac in validate_format:
+            self.assertTrue(self.constraint.validate(mac, None))
+
+    def test_invalid_mac_format(self):
+        invalidate_format = [
+            '8.8.8.8',
+            '0a-1b-3c-4d-5e-6f-1f',
+            '0a-1b-3c-4d-5e-xx'
+        ]
+        for mac in invalidate_format:
+            self.assertFalse(self.constraint.validate(mac, None))
+
+
+class TestCIDRConstraint(common.HeatTestCase):
+
+    def setUp(self):
+        super(TestCIDRConstraint, self).setUp()
+        self.constraint = cc.CIDRConstraint()
+
+    def test_valid_cidr_format(self):
+        validate_format = [
+            '10.0.0.0/24',
+            '6000::/64',
+            '8.8.8.8'
+        ]
+        for cidr in validate_format:
+            self.assertTrue(self.constraint.validate(cidr, None))
+
+    def test_invalid_cidr_format(self):
+        invalidate_format = [
+            '::/129',
+            'Invalid cidr',
+            '300.0.0.0/24',
+            '10.0.0.0/33',
+            '8.8.8.0/ 24'
+        ]
+        for cidr in invalidate_format:
+            self.assertFalse(self.constraint.validate(cidr, None))
+
+
 class TestISO8601Constraint(common.HeatTestCase):
 
     def setUp(self):
