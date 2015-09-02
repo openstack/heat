9cb27fe202f61576bb7b24cc9502cbe958d8e548
Angus Salkeld <asalkeld@mirantis.com>
2015-09-02 12:19:52 +1000

    Use the new (and default) glance v2 cli options
    Change-Id: Ib63017bedd1dbf49ab179bbf69da42ad35bcf1f8
    Closes-bug: #1491199

--- heat_integrationtests/prepare_test_env.sh
+++ heat_integrationtests/prepare_test_env.sh
@@ -31,7 +31,7 @@ nova flavor-create m1.heat_int 452 512 0 1
 nova flavor-create m1.heat_micro 453 128 0 1
 
 # Register the glance image for testing
-glance image-create --name fedora-heat-test-image --disk-format qcow2 --container-format bare --is-public True --location http://tarballs.openstack.org/heat-test-image/fedora-heat-test-image.qcow2
+curl http://tarballs.openstack.org/heat-test-image/fedora-heat-test-image.qcow2 | glance image-create --name fedora-heat-test-image --disk-format qcow2 --container-format bare --visibility public
 iniset heat_integrationtests.conf DEFAULT image_ref fedora-heat-test-image
 iniset heat_integrationtests.conf DEFAULT boot_config_env $DEST/heat-templates/hot/software-config/boot-config/test_image_env.yaml
 iniset heat_integrationtests.conf DEFAULT minimal_image_ref cirros-0.3.4-x86_64-uec
