4f8287d381d464dc91a3b7cd097158a39bb0bdd1
Steve Baker <sbaker@redhat.com>
2015-10-06 12:54:24 +1300

    Do no net/subnet checks during validate
    Currently validation fails if the network or the subnet are defined in
    the same template as the server because they don't exist during
    validate.
    
    _validate_belonging_subnet_to_net is called during _build_nics so this
    check still happens when the required resources actually exist.
    
    Also, test method validate_internal_port_subnet_not_this_network was not
    running because its name was not prefixed with test_. Fixing and
    updating this test revealed issues with the validation failed message.
    
    Closes-Bug: #1503060
    Change-Id: I2e062abf5b9fb7446b979ba6acc2e2b531d957d6

--- heat/engine/resources/openstack/nova/server_network_mixin.py
+++ heat/engine/resources/openstack/nova/server_network_mixin.py
@@ -20,7 +20,6 @@ from oslo_utils import netutils
 from heat.common import exception
 from heat.common.i18n import _
 from heat.common.i18n import _LI
-from heat.engine.cfn import functions as cfn_funcs
 
 LOG = logging.getLogger(__name__)
 
@@ -65,32 +64,20 @@ class ServerNetworkMixin(object):
                           network=network[self.NETWORK_ID],
                           server=self.name))
 
-        # If subnet and net are specified with some external resources, check
-        # them. Otherwise, if their are resources of current stack, skip
-        # validating in case of raising error and check it only during
-        # resource creating.
-        is_ref = False
-        for item in [subnet, net_uuid, net_id]:
-            if isinstance(item, (cfn_funcs.ResourceRef, cfn_funcs.GetAtt)):
-                is_ref = True
-                break
-        if not is_ref:
-            self._validate_belonging_subnet_to_net(network)
-
     def _validate_belonging_subnet_to_net(self, network):
         if network.get(self.NETWORK_PORT) is None and self.is_using_neutron():
             net = self._get_network_id(network)
             # check if there are subnet and network both specified that
             # subnet belongs to specified network
-            if (network.get(self.NETWORK_SUBNET) is not None
-               and (net is not None)):
+            subnet = network.get(self.NETWORK_SUBNET)
+            if (subnet is not None and net is not None):
                 subnet_net = self.client_plugin(
                     'neutron').network_id_from_subnet_id(
                     self._get_subnet_id(network))
                 if subnet_net != net:
-                    msg = _('Specified subnet %(subnet)s does not belongs to'
+                    msg = _('Specified subnet %(subnet)s does not belongs to '
                             'network %(network)s.') % {
-                        'subnet': subnet_net,
+                        'subnet': subnet,
                         'network': net}
                     raise exception.StackValidationFailed(message=msg)
 
