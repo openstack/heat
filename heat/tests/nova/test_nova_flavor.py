74464cf37483f449e18f2168d2c3424ec20426a7
Rabi Mishra <ramishra@redhat.com>
2015-11-09 13:01:24 +0530

    Add `is_public` property to Nova Flavor resource
    This adds `is_public` property/attribute to OS::Nova::Flavor
    Resource.
    
    Change-Id: Ia356d6217e13e77ccc13644a593988685d967632
    Closes-Bug: #1514398

--- heat/tests/nova/test_nova_flavor.py
+++ heat/tests/nova/test_nova_flavor.py
@@ -66,10 +66,25 @@ class NovaFlavorTest(common.HeatTestCase):
         value = mock.MagicMock()
         flavor_id = '927202df-1afb-497f-8368-9c2d2f26e5db'
         value.id = flavor_id
+        value.is_public = True
         self.flavors.create.return_value = value
+        self.flavors.get.return_value = value
         self.my_flavor.handle_create()
         value.set_keys.assert_called_once_with({"foo": "bar"})
         self.assertEqual(flavor_id, self.my_flavor.resource_id)
+        self.assertTrue(self.my_flavor.FnGetAtt('is_public'))
+
+    def test_private_flavor_handle_create(self):
+        value = mock.MagicMock()
+        flavor_id = '927202df-1afb-497f-8368-9c2d2f26e5db'
+        value.id = flavor_id
+        value.is_public = False
+        self.flavors.create.return_value = value
+        self.flavors.get.return_value = value
+        self.my_flavor.handle_create()
+        value.set_keys.assert_called_once_with({"foo": "bar"})
+        self.assertEqual(flavor_id, self.my_flavor.resource_id)
+        self.assertFalse(self.my_flavor.FnGetAtt('is_public'))
 
     def test_flavor_handle_update_keys(self):
         value = mock.MagicMock()
