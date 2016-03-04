openstack/heat

commit a7c4b2332f9d1accaa89b51fbc3b09014995983a

Should execute 'resolve' before check the values of 'for_each'
Change-Id: I75f58885a91d11e52ed3737a806b5920774d586c
Closes-Bug: #1466748

heat/engine/hot/functions.py
@@ -301,7 +301,7 @@ class Repeat(function.Function):
                             self.fn_name)
 
         try:
-            for_each = self.args['for_each']
+            for_each = function.resolve(self.args['for_each'])
             template = self.args['template']
         except (KeyError, TypeError):
             example = ('''repeat:
@@ -335,9 +335,8 @@ class Repeat(function.Function):
                         for (k, v) in template.items())
 
     def result(self):
-        for_each = function.resolve(self._for_each)
-        keys = list(six.iterkeys(for_each))
-        lists = [for_each[key] for key in keys]
+        keys = list(six.iterkeys(self._for_each))
+        lists = [self._for_each[key] for key in keys]
         template = function.resolve(self._template)
         return [self._do_replacement(keys, items, template)
                 for items in itertools.product(*lists)]

heat/tests/test_hot.py
@@ -593,13 +593,21 @@ class HOTemplateTest(common.HeatTestCase):
 
     def test_repeat(self):
         """Test repeat function."""
-        snippet = {'repeat': {'template': 'this is %var%',
-                              'for_each': {'%var%': ['a', 'b', 'c']}}}
+        hot_tpl = template_format.parse('''
+        heat_template_version: 2015-04-30
+        parameters:
+          param:
+            type: comma_delimited_list
+            default: 'a,b,c'
+        ''')
+        snippet = {'repeat': {'template': 'this is var%',
+                   'for_each': {'var%': {'get_param': 'param'}}}}
         snippet_resolved = ['this is a', 'this is b', 'this is c']
 
-        tmpl = template.Template(hot_kilo_tpl_empty)
+        tmpl = template.Template(hot_tpl)
+        stack = parser.Stack(utils.dummy_context(), 'test_stack', tmpl)
 
-        self.assertEqual(snippet_resolved, self.resolve(snippet, tmpl))
+        self.assertEqual(snippet_resolved, self.resolve(snippet, tmpl, stack))
 
     def test_repeat_dict_template(self):
         """Test repeat function with a dictionary as a template."""

commit bf60398c906b3140553976513e9f08ecc44433c8

Resolve arguments to 'repeat' function at runtime
This fixes a regression caused by a7c4b2332f9d1accaa89b51fbc3b09014995983a
in evaluating the 'for_each' argument too early (at parse time - before the
result of runtime functions like get_attr are available), while preserving
the fix for bug 1466748 in allowing intrinsic functions to be used in the
argument.

Change-Id: If1e9c754ef372de2f7edd32a9a8247eb271c5871
Closes-Bug: #1553306
Related-Bug: #1466748

heat/engine/hot/functions.py
@@ -530,7 +530,7 @@ class Repeat(function.Function):
                             self.fn_name)
 
         try:
-            for_each = function.resolve(self.args['for_each'])
+            for_each = self.args['for_each']
             template = self.args['template']
         except (KeyError, TypeError):
             example = ('''repeat:
@@ -540,13 +540,14 @@ class Repeat(function.Function):
             raise KeyError(_('"repeat" syntax should be %s') %
                            example)
 
-        if not isinstance(for_each, collections.Mapping):
-            raise TypeError(_('The "for_each" argument to "%s" must contain '
-                              'a map') % self.fn_name)
-        for v in six.itervalues(for_each):
-            if not isinstance(v, list):
-                raise TypeError(_('The values of the "for_each" argument to '
-                                  '"%s" must be lists') % self.fn_name)
+        if not isinstance(for_each, function.Function):
+            if not isinstance(for_each, collections.Mapping):
+                raise TypeError(_('The "for_each" argument to "%s" must '
+                                  'contain a map') % self.fn_name)
+            for v in six.itervalues(for_each):
+                if not isinstance(v, (list, function.Function)):
+                    raise TypeError(_('The values of the "for_each" argument '
+                                      'to "%s" must be lists') % self.fn_name)
 
         return for_each, template
 
@@ -564,9 +565,15 @@ class Repeat(function.Function):
                         for (k, v) in template.items())
 
     def result(self):
-        keys = list(six.iterkeys(self._for_each))
-        lists = [self._for_each[key] for key in keys]
+        for_each = function.resolve(self._for_each)
+        keys = list(six.iterkeys(for_each))
+        lists = [for_each[key] for key in keys]
+        if not all(isinstance(l, list) for l in lists):
+            raise TypeError(_('The values of the "for_each" argument to '
+                              '"%s" must be lists') % self.fn_name)
+
         template = function.resolve(self._template)
+
         return [self._do_replacement(keys, items, template)
                 for items in itertools.product(*lists)]
 

heat/tests/test_hot.py
@@ -835,6 +835,16 @@ class HOTemplateTest(common.HeatTestCase):
 
     def test_repeat(self):
         """Test repeat function."""
+        snippet = {'repeat': {'template': 'this is %var%',
+                              'for_each': {'%var%': ['a', 'b', 'c']}}}
+        snippet_resolved = ['this is a', 'this is b', 'this is c']
+
+        tmpl = template.Template(hot_kilo_tpl_empty)
+
+        self.assertEqual(snippet_resolved, self.resolve(snippet, tmpl))
+
+    def test_repeat_get_param(self):
+        """Test repeat function with get_param function as an argument."""
         hot_tpl = template_format.parse('''
         heat_template_version: 2015-04-30
         parameters:
@@ -1196,6 +1206,22 @@ class HotStackTest(common.HeatTestCase):
 
     def resolve(self, snippet):
         return function.resolve(self.stack.t.parse(self.stack, snippet))
+
+    def test_repeat_get_attr(self):
+        """Test repeat function with get_attr function as an argument."""
+        tmpl = template.Template(hot_tpl_complex_attrs_all_attrs)
+        self.stack = parser.Stack(self.ctx, 'test_repeat_get_attr', tmpl)
+
+        snippet = {'repeat': {'template': 'this is %var%',
+                   'for_each': {'%var%': {'get_attr': ['resource1', 'list']}}}}
+        repeat = self.stack.t.parse(self.stack, snippet)
+
+        self.stack.store()
+        self.stack.create()
+        self.assertEqual((parser.Stack.CREATE, parser.Stack.COMPLETE),
+                         self.stack.state)
+        self.assertEqual(['this is foo', 'this is bar'],
+                         function.resolve(repeat))
 
     def test_get_attr_multiple_rsrc_status(self):
         """Test resolution of get_attr occurrences in HOT template."""