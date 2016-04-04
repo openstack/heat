96cec395b92d12ff9e2bad0dfb45fa2575cea6e7
Peter Razumovsky <prazumovsky@mirantis.com>
2016-04-04 13:35:19 +0300

    Keystone::Region observe reality implementation
    Add override parse_live_resource_data method implementation for
    OS::Keystone::Region resource.
    
    Also, translation rule resolve has been added for parent_region
    property.
    
    implements bp get-reality-for-resources
    
    Change-Id: I5f480cdacffb639e1d176ea5c8e9e13dbe24ff83

--- heat/tests/openstack/keystone/test_region.py
+++ heat/tests/openstack/keystone/test_region.py
@@ -145,3 +145,23 @@ class KeystoneRegionTest(common.HeatTestCase):
             enabled=prop_diff[region.KeystoneRegion.ENABLED],
             parent_region='test_parent_region'
         )
+
+    def test_region_get_live_state(self):
+        self.test_region.resource_id = '477e8273-60a7-4c41-b683-fdb0bc7cd151'
+        mock_dict = mock.MagicMock()
+        mock_dict.to_dict.return_value = {
+            "parent_region_id": None,
+            "enabled": True,
+            "id": "79e4d02f8b454a7885c413d5d4297813",
+            "links": {"self": "link"},
+            "description": ""
+        }
+        self.regions.get.return_value = mock_dict
+
+        reality = self.test_region.get_live_state(self.test_region.properties)
+        expected = {
+            "parent_region": None,
+            "enabled": True,
+            "description": ""
+        }
+        self.assertEqual(expected, reality)
