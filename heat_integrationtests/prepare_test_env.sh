c3ac439ed232642134778f980bb82e5aeb783bb4
Thomas Herve <therve@redhat.com>
2016-07-06 21:43:57 +0200

    Use Fedora 24 base image
    This is to fix mitaka CI, which is failing since the heat-test-image
    rebuild failed.
    
    Closes-Bug: #1534026
    (cherry picked from commits 150a013597da2605be34a3971430283fc25946da
                            and 59fc53a66c4dec45e4d150bce0a1d674477f710c)
    Change-Id: Ifaccf340f0ff4eac52b1eeef5914b7e90f2687da

--- heat_integrationtests/prepare_test_env.sh
+++ heat_integrationtests/prepare_test_env.sh
@@ -31,7 +31,13 @@ nova flavor-create m1.heat_int 452 512 0 1
 nova flavor-create m1.heat_micro 453 128 0 1
 
 # Register the glance image for testing
-curl http://tarballs.openstack.org/heat-test-image/fedora-heat-test-image.qcow2 | glance image-create --name fedora-heat-test-image --disk-format qcow2 --container-format bare --visibility public
+curl -L https://download.fedoraproject.org/pub/fedora/linux/releases/24/CloudImages/x86_64/images/Fedora-Cloud-Base-24-1.2.x86_64.qcow2 | openstack image create fedora-heat-test-image --disk-format qcow2 --container-format bare --public
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+   # The curl command failed, so the upload is mostly likely incorrect. Let's
+   # bail out early.
+   exit 1
+fi
+
 iniset heat_integrationtests.conf DEFAULT image_ref fedora-heat-test-image
 iniset heat_integrationtests.conf DEFAULT boot_config_env $DEST/heat-templates/hot/software-config/boot-config/test_image_env.yaml
 iniset heat_integrationtests.conf DEFAULT heat_config_notify_script $DEST/heat-templates/hot/software-config/elements/heat-config/bin/heat-config-notify
