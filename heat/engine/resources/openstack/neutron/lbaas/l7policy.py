897177bed66dd7a05a1bd0638b4c351d937ad871
Dmitriy Uvarenkov <duvarenkov@mirantis.com>
2016-07-06 16:28:14 +0300

    Add new resource l7policy
    Add support of neutron lbaas l7policy.
    
    Partial-bug: #1563041
    Partially implements: bp lbaas-l7-rules-support
    
    Change-Id: Ie46bf6c9e1ddbd737a88c7d2680eae5eaa18dfca

---
+++ heat/engine/resources/openstack/neutron/lbaas/l7policy.py
@@ -0,0 +1,271 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from heat.common import exception
+from heat.common.i18n import _
+from heat.engine import attributes
+from heat.engine import constraints
+from heat.engine import properties
+from heat.engine.resources.openstack.neutron import neutron
+from heat.engine import support
+from heat.engine import translation
+
+
+class L7Policy(neutron.NeutronResource):
+    """A resource for managing LBaaS v2 L7Policies.
+
+    This resource manages Neutron-LBaaS v2 L7Policies, which represent
+    a collection of L7Rules. L7Policy holds the action that should be performed
+    when the rules are matched (Redirect to Pool, Redirect to URL, Reject).
+    L7Policy holds a Listener id, so a Listener can evaluate a collection of
+    L7Policies. L7Policy will return True when all of the L7Rules that belong
+    to this L7Policy are matched. L7Policies under a specific Listener are
+    ordered and the first l7Policy that returns a match will be executed.
+    When none of the policies match the request gets forwarded to
+    listener.default_pool_id.
+    """
+
+    support_status = support.SupportStatus(version='7.0.0')
+
+    required_service_extension = 'lbaasv2'
+
+    PROPERTIES = (
+        NAME, DESCRIPTION, ADMIN_STATE_UP, ACTION,
+        REDIRECT_POOL, REDIRECT_URL, POSITION, LISTENER
+    ) = (
+        'name', 'description', 'admin_state_up', 'action',
+        'redirect_pool', 'redirect_url', 'position', 'listener'
+    )
+
+    L7ACTIONS = (
+        REJECT, REDIRECT_TO_POOL, REDIRECT_TO_URL
+    ) = (
+        'REJECT', 'REDIRECT_TO_POOL', 'REDIRECT_TO_URL'
+    )
+
+    ATTRIBUTES = (RULES_ATTR) = ('rules')
+
+    properties_schema = {
+        NAME: properties.Schema(
+            properties.Schema.STRING,
+            _('Name of the policy.'),
+            update_allowed=True
+        ),
+        DESCRIPTION: properties.Schema(
+            properties.Schema.STRING,
+            _('Description of the policy.'),
+            update_allowed=True
+        ),
+        ADMIN_STATE_UP: properties.Schema(
+            properties.Schema.BOOLEAN,
+            _('The administrative state of the policy.'),
+            default=True,
+            update_allowed=True
+        ),
+        ACTION: properties.Schema(
+            properties.Schema.STRING,
+            _('Action type of the policy.'),
+            required=True,
+            constraints=[constraints.AllowedValues(L7ACTIONS)],
+            update_allowed=True
+        ),
+        REDIRECT_POOL: properties.Schema(
+            properties.Schema.STRING,
+            _('ID or name of the pool for REDIRECT_TO_POOL action type.'),
+            constraints=[
+                constraints.CustomConstraint('neutron.lbaas.pool')
+            ],
+            update_allowed=True
+        ),
+        REDIRECT_URL: properties.Schema(
+            properties.Schema.STRING,
+            _('URL for REDIRECT_TO_URL action type. '
+              'This should be a valid URL string.'),
+            update_allowed=True
+        ),
+        POSITION: properties.Schema(
+            properties.Schema.NUMBER,
+            _('L7 policy position in ordered policies list. This must be '
+              'an integer starting from 1. If not specified, policy will be '
+              'placed at the tail of existing policies list.'),
+            constraints=[constraints.Range(min=1)],
+            update_allowed=True
+        ),
+        LISTENER: properties.Schema(
+            properties.Schema.STRING,
+            _('ID or name of the listener this policy belongs to.'),
+            required=True,
+            constraints=[
+                constraints.CustomConstraint('neutron.lbaas.listener')
+            ]
+        ),
+    }
+
+    attributes_schema = {
+        RULES_ATTR: attributes.Schema(
+            _('L7Rules associated with this policy.'),
+            type=attributes.Schema.LIST
+        ),
+    }
+
+    def translation_rules(self, props):
+        return [
+            translation.TranslationRule(
+                props,
+                translation.TranslationRule.RESOLVE,
+                [self.LISTENER],
+                client_plugin=self.client_plugin(),
+                finder='find_resourceid_by_name_or_id',
+                entity='listener'
+            ),
+        ]
+
+    def __init__(self, name, definition, stack):
+        super(L7Policy, self).__init__(name, definition, stack)
+        self._lb_id = None
+
+    @property
+    def lb_id(self):
+        if self._lb_id is None:
+            listener_id = self.properties[self.LISTENER]
+            listener = self.client().show_listener(listener_id)['listener']
+            self._lb_id = listener['loadbalancers'][0]['id']
+        return self._lb_id
+
+    def validate(self):
+        res = super(L7Policy, self).validate()
+        if res:
+            return res
+
+        if (self.properties[self.ACTION] == self.REJECT and
+                (self.properties[self.REDIRECT_POOL] is not None or
+                 self.properties[self.REDIRECT_URL] is not None)):
+            msg = (_('Properties %(pool)s and %(url)s are not required when '
+                     '%(action)s type is set to %(action_type)s.') %
+                   {'pool': self.REDIRECT_POOL,
+                    'url': self.REDIRECT_URL,
+                    'action': self.ACTION,
+                    'action_type': self.REJECT})
+            raise exception.StackValidationFailed(message=msg)
+
+        if self.properties[self.ACTION] == self.REDIRECT_TO_POOL:
+            if self.properties[self.REDIRECT_URL] is not None:
+                raise exception.ResourcePropertyValueDependency(
+                    prop1=self.REDIRECT_URL,
+                    prop2=self.ACTION,
+                    value=self.REDIRECT_TO_URL)
+            if self.properties[self.REDIRECT_POOL] is None:
+                msg = (_('Property %(pool)s is required when %(action)s '
+                         'type is set to %(action_type)s.') %
+                       {'pool': self.REDIRECT_POOL,
+                        'action': self.ACTION,
+                        'action_type': self.REDIRECT_TO_POOL})
+                raise exception.StackValidationFailed(message=msg)
+
+        if self.properties[self.ACTION] == self.REDIRECT_TO_URL:
+            if self.properties[self.REDIRECT_POOL] is not None:
+                raise exception.ResourcePropertyValueDependency(
+                    prop1=self.REDIRECT_POOL,
+                    prop2=self.ACTION,
+                    value=self.REDIRECT_TO_POOL)
+            if self.properties[self.REDIRECT_URL] is None:
+                msg = (_('Property %(url)s is required when %(action)s '
+                         'type is set to %(action_type)s.') %
+                       {'url': self.REDIRECT_URL,
+                        'action': self.ACTION,
+                        'action_type': self.REDIRECT_TO_URL})
+                raise exception.StackValidationFailed(message=msg)
+
+    def _check_lb_status(self):
+        return self.client_plugin().check_lb_status(self.lb_id)
+
+    def handle_create(self):
+        properties = self.prepare_properties(
+            self.properties,
+            self.physical_resource_name())
+
+        properties['listener_id'] = properties.pop(self.LISTENER)
+        if self.properties[self.REDIRECT_POOL] is not None:
+            self.client_plugin().resolve_pool(
+                properties, self.REDIRECT_POOL, 'redirect_pool_id')
+
+        return properties
+
+    def check_create_complete(self, properties):
+        if self.resource_id is None:
+            try:
+                l7policy = self.client().create_lbaas_l7policy(
+                    {'l7policy': properties})['l7policy']
+                self.resource_id_set(l7policy['id'])
+            except Exception as ex:
+                if self.client_plugin().is_invalid(ex):
+                    return False
+                raise
+
+        return self._check_lb_status()
+
+    def _show_resource(self):
+        return self.client().show_lbaas_l7policy(self.resource_id)['l7policy']
+
+    def handle_update(self, json_snippet, tmpl_diff, prop_diff):
+        self._update_called = False
+        if self.REDIRECT_POOL in prop_diff:
+            if prop_diff[self.REDIRECT_POOL] is not None:
+                self.client_plugin().resolve_pool(
+                    prop_diff, self.REDIRECT_POOL, 'redirect_pool_id')
+            else:
+                prop_diff.pop(self.REDIRECT_POOL)
+                prop_diff['redirect_pool_id'] = None
+        return prop_diff
+
+    def check_update_complete(self, prop_diff):
+        if not prop_diff:
+            return True
+
+        if not self._update_called:
+            try:
+                self.client().update_lbaas_l7policy(
+                    self.resource_id, {'l7policy': prop_diff})
+                self._update_called = True
+            except Exception as ex:
+                if self.client_plugin().is_invalid(ex):
+                    return False
+                raise
+
+        return self._check_lb_status()
+
+    def handle_delete(self):
+        self._delete_called = False
+
+    def check_delete_complete(self, data):
+        if self.resource_id is None:
+            return True
+
+        if not self._delete_called:
+            try:
+                self.client().delete_lbaas_l7policy(self.resource_id)
+                self._delete_called = True
+            except Exception as ex:
+                if self.client_plugin().is_invalid(ex):
+                    return False
+                elif self.client_plugin().is_not_found(ex):
+                    return True
+                raise
+
+        return self._check_lb_status()
+
+
+def resource_mapping():
+    return {
+        'OS::Neutron::LBaaS::L7Policy': L7Policy
+    }
---
+++ heat/tests/openstack/neutron/lbaas/test_l7policy.py
@@ -0,0 +1,255 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+import yaml
+
+from neutronclient.common import exceptions
+
+from heat.common import exception
+from heat.common.i18n import _
+from heat.common import template_format
+from heat.engine.resources.openstack.neutron.lbaas import l7policy
+from heat.tests import common
+from heat.tests.openstack.neutron import inline_templates
+from heat.tests import utils
+
+
+class L7PolicyTest(common.HeatTestCase):
+
+    def test_resource_mapping(self):
+        mapping = l7policy.resource_mapping()
+        self.assertEqual(mapping['OS::Neutron::LBaaS::L7Policy'],
+                         l7policy.L7Policy)
+
+    @mock.patch('heat.engine.clients.os.neutron.'
+                'NeutronClientPlugin.has_extension', return_value=True)
+    def _create_stack(self, ext_func, tmpl=inline_templates.L7POLICY_TEMPLATE):
+        self.t = template_format.parse(tmpl)
+        self.stack = utils.parse_stack(self.t)
+        self.l7policy = self.stack['l7policy']
+
+        self.neutron_client = mock.MagicMock()
+        self.l7policy.client = mock.MagicMock(return_value=self.neutron_client)
+
+        self.l7policy.client_plugin().find_resourceid_by_name_or_id = (
+            mock.MagicMock(return_value='123'))
+        self.l7policy.client_plugin().client = mock.MagicMock(
+            return_value=self.neutron_client)
+        self.neutron_client.show_loadbalancer.side_effect = [
+            {'loadbalancer': {'provisioning_status': 'PENDING_UPDATE'}},
+            {'loadbalancer': {'provisioning_status': 'PENDING_UPDATE'}},
+            {'loadbalancer': {'provisioning_status': 'ACTIVE'}},
+        ]
+
+    def test_validate_reject_action_with_conflicting_props(self):
+        tmpl = yaml.load(inline_templates.L7POLICY_TEMPLATE)
+        props = tmpl['resources']['l7policy']['properties']
+        props['action'] = 'REJECT'
+        self._create_stack(tmpl=yaml.dump(tmpl))
+
+        msg = _('Properties redirect_pool and redirect_url are not '
+                'required when action type is set to REJECT.')
+        with mock.patch('heat.engine.clients.os.neutron.NeutronClientPlugin.'
+                        'has_extension', return_value=True):
+            self.assertRaisesRegexp(exception.StackValidationFailed,
+                                    msg, self.l7policy.validate)
+
+    def test_validate_redirect_pool_action_with_url(self):
+        tmpl = yaml.load(inline_templates.L7POLICY_TEMPLATE)
+        props = tmpl['resources']['l7policy']['properties']
+        props['action'] = 'REDIRECT_TO_POOL'
+        props['redirect_pool'] = '123'
+        self._create_stack(tmpl=yaml.dump(tmpl))
+
+        msg = _('redirect_url property should only be specified '
+                'for action with value REDIRECT_TO_URL.')
+        with mock.patch('heat.engine.clients.os.neutron.NeutronClientPlugin.'
+                        'has_extension', return_value=True):
+            self.assertRaisesRegexp(exception.ResourcePropertyValueDependency,
+                                    msg, self.l7policy.validate)
+
+    def test_validate_redirect_pool_action_without_pool(self):
+        tmpl = yaml.load(inline_templates.L7POLICY_TEMPLATE)
+        props = tmpl['resources']['l7policy']['properties']
+        props['action'] = 'REDIRECT_TO_POOL'
+        del props['redirect_url']
+        self._create_stack(tmpl=yaml.dump(tmpl))
+
+        msg = _('Property redirect_pool is required when action type '
+                'is set to REDIRECT_TO_POOL.')
+        with mock.patch('heat.engine.clients.os.neutron.NeutronClientPlugin.'
+                        'has_extension', return_value=True):
+            self.assertRaisesRegexp(exception.StackValidationFailed,
+                                    msg, self.l7policy.validate)
+
+    def test_validate_redirect_url_action_with_pool(self):
+        tmpl = yaml.load(inline_templates.L7POLICY_TEMPLATE)
+        props = tmpl['resources']['l7policy']['properties']
+        props['redirect_pool'] = '123'
+        self._create_stack(tmpl=yaml.dump(tmpl))
+
+        msg = _('redirect_pool property should only be specified '
+                'for action with value REDIRECT_TO_POOL.')
+        with mock.patch('heat.engine.clients.os.neutron.NeutronClientPlugin.'
+                        'has_extension', return_value=True):
+            self.assertRaisesRegexp(exception.ResourcePropertyValueDependency,
+                                    msg, self.l7policy.validate)
+
+    def test_validate_redirect_url_action_without_url(self):
+        tmpl = yaml.load(inline_templates.L7POLICY_TEMPLATE)
+        props = tmpl['resources']['l7policy']['properties']
+        del props['redirect_url']
+        self._create_stack(tmpl=yaml.dump(tmpl))
+
+        msg = _('Property redirect_url is required when action type '
+                'is set to REDIRECT_TO_URL.')
+        with mock.patch('heat.engine.clients.os.neutron.NeutronClientPlugin.'
+                        'has_extension', return_value=True):
+            self.assertRaisesRegexp(exception.StackValidationFailed,
+                                    msg, self.l7policy.validate)
+
+    def test_create(self):
+        self._create_stack()
+        self.neutron_client.create_lbaas_l7policy.side_effect = [
+            exceptions.StateInvalidClient,
+            {'l7policy': {'id': '1234'}}
+        ]
+        expected = {
+            'l7policy': {
+                'name': u'test_l7policy',
+                'description': u'test l7policy resource',
+                'action': u'REDIRECT_TO_URL',
+                'listener_id': u'123',
+                'redirect_url': u'http://www.mirantis.com',
+                'position': 1,
+                'admin_state_up': True
+            }
+        }
+
+        props = self.l7policy.handle_create()
+
+        self.assertFalse(self.l7policy.check_create_complete(props))
+        self.neutron_client.create_lbaas_l7policy.assert_called_with(expected)
+        self.assertFalse(self.l7policy.check_create_complete(props))
+        self.neutron_client.create_lbaas_l7policy.assert_called_with(expected)
+        self.assertFalse(self.l7policy.check_create_complete(props))
+        self.assertTrue(self.l7policy.check_create_complete(props))
+
+    def test_create_missing_properties(self):
+        for prop in ('action', 'listener'):
+            tmpl = yaml.load(inline_templates.L7POLICY_TEMPLATE)
+            del tmpl['resources']['l7policy']['properties'][prop]
+            self._create_stack(tmpl=yaml.dump(tmpl))
+
+            self.assertRaises(exception.StackValidationFailed,
+                              self.l7policy.validate)
+
+    def test_show_resource(self):
+        self._create_stack()
+        self.l7policy.resource_id_set('1234')
+        self.neutron_client.show_lbaas_l7policy.return_value = {
+            'l7policy': {'id': '1234'}
+        }
+
+        self.assertEqual({'id': '1234'}, self.l7policy._show_resource())
+
+        self.neutron_client.show_lbaas_l7policy.assert_called_with('1234')
+
+    def test_update(self):
+        self._create_stack()
+        self.l7policy.resource_id_set('1234')
+        self.neutron_client.update_lbaas_l7policy.side_effect = [
+            exceptions.StateInvalidClient, None]
+        prop_diff = {
+            'admin_state_up': False,
+            'name': 'your_l7policy',
+            'redirect_url': 'http://www.google.com'
+        }
+
+        prop_diff = self.l7policy.handle_update(None, None, prop_diff)
+
+        self.assertFalse(self.l7policy.check_update_complete(prop_diff))
+        self.assertFalse(self.l7policy._update_called)
+        self.neutron_client.update_lbaas_l7policy.assert_called_with(
+            '1234', {'l7policy': prop_diff})
+        self.assertFalse(self.l7policy.check_update_complete(prop_diff))
+        self.assertTrue(self.l7policy._update_called)
+        self.neutron_client.update_lbaas_l7policy.assert_called_with(
+            '1234', {'l7policy': prop_diff})
+        self.assertFalse(self.l7policy.check_update_complete(prop_diff))
+        self.assertTrue(self.l7policy.check_update_complete(prop_diff))
+
+    def test_update_redirect_pool_prop_name(self):
+        self._create_stack()
+        self.l7policy.resource_id_set('1234')
+        self.neutron_client.update_lbaas_l7policy.side_effect = [
+            exceptions.StateInvalidClient, None]
+        unresolved_diff = {
+            'redirect_url': None,
+            'action': 'REDIRECT_TO_POOL',
+            'redirect_pool': 'UNRESOLVED_POOL'
+        }
+        resolved_diff = {
+            'redirect_url': None,
+            'action': 'REDIRECT_TO_POOL',
+            'redirect_pool_id': '123'
+        }
+
+        self.l7policy.handle_update(None, None, unresolved_diff)
+
+        self.assertFalse(self.l7policy.check_update_complete(resolved_diff))
+        self.assertFalse(self.l7policy._update_called)
+        self.neutron_client.update_lbaas_l7policy.assert_called_with(
+            '1234', {'l7policy': resolved_diff})
+        self.assertFalse(self.l7policy.check_update_complete(resolved_diff))
+        self.assertTrue(self.l7policy._update_called)
+        self.neutron_client.update_lbaas_l7policy.assert_called_with(
+            '1234', {'l7policy': resolved_diff})
+        self.assertFalse(self.l7policy.check_update_complete(resolved_diff))
+        self.assertTrue(self.l7policy.check_update_complete(resolved_diff))
+
+    def test_delete(self):
+        self._create_stack()
+        self.l7policy.resource_id_set('1234')
+        self.neutron_client.delete_lbaas_l7policy.side_effect = [
+            exceptions.StateInvalidClient, None]
+
+        self.l7policy.handle_delete()
+
+        self.assertFalse(self.l7policy.check_delete_complete(None))
+        self.assertFalse(self.l7policy._delete_called)
+        self.assertFalse(self.l7policy.check_delete_complete(None))
+        self.assertTrue(self.l7policy._delete_called)
+        self.neutron_client.delete_lbaas_l7policy.assert_called_with('1234')
+        self.assertFalse(self.l7policy.check_delete_complete(None))
+        self.assertTrue(self.l7policy.check_delete_complete(None))
+
+    def test_delete_already_gone(self):
+        self._create_stack()
+        self.l7policy.resource_id_set('1234')
+        self.neutron_client.delete_lbaas_l7policy.side_effect = (
+            exceptions.NotFound)
+
+        self.l7policy.handle_delete()
+        self.assertTrue(self.l7policy.check_delete_complete(None))
+
+    def test_delete_failed(self):
+        self._create_stack()
+        self.l7policy.resource_id_set('1234')
+        self.neutron_client.delete_lbaas_l7policy.side_effect = (
+            exceptions.Unauthorized)
+
+        self.l7policy.handle_delete()
+        self.assertRaises(exceptions.Unauthorized,
+                          self.l7policy.check_delete_complete, None)
