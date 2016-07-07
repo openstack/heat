7f8e3ff4c1b4ab1f5fe36ced62f98b0d821a160a
Dmitriy Uvarenkov <duvarenkov@mirantis.com>
2016-07-07 18:07:08 +0300

    Aodh::Alarm observe reality implementation
    Override parse_live_resource_data method for Aodh::Alarm
    resource to observe and update resource plugin with live state.
    
    implements bp get-reality-for-resources
    
    Change-Id: I55e08fbbc6e485808f89375cb0e114f6d6795980

--- heat/tests/openstack/aodh/test_alarm.py
+++ heat/tests/openstack/aodh/test_alarm.py
@@ -596,6 +596,52 @@ class AodhAlarmTest(common.HeatTestCase):
             % (timezone, timezone),
             error.message)
 
+    def test_alarm_live_state(self):
+        snippet = template_format.parse(alarm_template)
+        self.stack = utils.parse_stack(snippet)
+        resource_defns = self.stack.t.resource_definitions(self.stack)
+        self.rsrc_defn = resource_defns['MEMAlarmHigh']
+        self.client = mock.Mock()
+        self.patchobject(alarm.AodhAlarm, 'client',
+                         return_value=self.client)
+
+        alarm_res = alarm.AodhAlarm('alarm', self.rsrc_defn, self.stack)
+        alarm_res.create()
+        value = {
+            'description': 'Scale-up if MEM > 50% for 1 minute',
+            'alarm_actions': [],
+            'time_constraints': [],
+            'threshold_rule': {
+                'meter_name': 'MemoryUtilization',
+                'statistic': 'avg',
+                'period': '60',
+                'evaluation_periods': '1',
+                'threshold': '50',
+                'matching_metadata': {},
+                'comparison_operator': 'gt',
+                'query': [{'field': 'c', 'op': 'ne', 'value': 'z'}]
+            }
+        }
+        self.client.alarm.get.return_value = value
+        expected_data = {
+            'description': 'Scale-up if MEM > 50% for 1 minute',
+            'alarm_actions': [],
+            'statistic': 'avg',
+            'period': '60',
+            'evaluation_periods': '1',
+            'threshold': '50',
+            'matching_metadata': {},
+            'comparison_operator': 'gt',
+            'query': [{'field': 'c', 'op': 'ne', 'value': 'z'}],
+            'repeat_actions': None,
+            'ok_actions': None,
+            'insufficient_data_actions': None,
+            'severity': None,
+            'enabled': None
+        }
+        reality = alarm_res.get_live_state(alarm_res.properties)
+        self.assertEqual(expected_data, reality)
+
 
 class CombinationAlarmTest(common.HeatTestCase):
 
