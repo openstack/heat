6aa49677b36665dde683328d4e50a37c42d95b9d
Dmitriy Uvarenkov <duvarenkov@mirantis.com>
2016-07-18 12:58:34 +0300

    Aodh::Gnocchi resources observe reality implementation
    Override parse_live_resource_data method for
    Aodh::Gnocchi resources to observe and
    update resource plugin with live state.
    
    implements bp get-reality-for-resources
    
    Change-Id: Ibac58b4c638b8edd29f51f79021226c142e83781

--- heat/tests/openstack/aodh/test_gnocchi_alarm.py
+++ heat/tests/openstack/aodh/test_gnocchi_alarm.py
@@ -207,6 +207,54 @@ class GnocchiResourcesAlarmTest(common.HeatTestCase):
         scheduler.TaskRunner(res.create)()
         self.assertEqual(FakeAodhAlarm, res.FnGetAtt('show'))
 
+    def test_gnocchi_alarm_live_state(self):
+        snippet = template_format.parse(gnocchi_resources_alarm_template)
+        self.stack = utils.parse_stack(snippet)
+        resource_defns = self.stack.t.resource_definitions(self.stack)
+        self.rsrc_defn = resource_defns['GnoResAlarm']
+        self.client = mock.Mock()
+        self.patchobject(gnocchi.AodhGnocchiResourcesAlarm, 'client',
+                         return_value=self.client)
+
+        alarm_res = gnocchi.AodhGnocchiResourcesAlarm(
+            'alarm', self.rsrc_defn, self.stack)
+        alarm_res.create()
+        value = {
+            'description': 'Do stuff with gnocchi',
+            'alarm_actions': [],
+            'time_constraints': [],
+            'gnocchi_resources_threshold_rule': {
+                'resource_id': '5a517ceb-b068-4aca-9eb9-3e4eb9b90d9a',
+                'metric': 'cpu_util',
+                'evaluation_periods': 1,
+                'aggregation_method': 'mean',
+                'granularity': 60,
+                'threshold': 50,
+                'comparison_operator': 'gt',
+                'resource_type': 'instance'
+            }
+        }
+        self.client.alarm.get.return_value = value
+        expected_data = {
+            'description': 'Do stuff with gnocchi',
+            'alarm_actions': [],
+            'resource_id': '5a517ceb-b068-4aca-9eb9-3e4eb9b90d9a',
+            'metric': 'cpu_util',
+            'evaluation_periods': 1,
+            'aggregation_method': 'mean',
+            'granularity': 60,
+            'threshold': 50,
+            'comparison_operator': 'gt',
+            'resource_type': 'instance',
+            'insufficient_data_actions': None,
+            'enabled': None,
+            'ok_actions': None,
+            'repeat_actions': None,
+            'severity': None
+        }
+        reality = alarm_res.get_live_state(alarm_res.properties)
+        self.assertEqual(expected_data, reality)
+
 
 class GnocchiAggregationByMetricsAlarmTest(GnocchiResourcesAlarmTest):
 
@@ -283,6 +331,53 @@ class GnocchiAggregationByMetricsAlarmTest(GnocchiResourcesAlarmTest):
         scheduler.TaskRunner(res.create)()
         self.assertEqual(FakeAodhAlarm, res.FnGetAtt('show'))
 
+    def test_gnocchi_alarm_aggr_by_metrics_live_state(self):
+        snippet = template_format.parse(
+            gnocchi_aggregation_by_metrics_alarm_template)
+        self.stack = utils.parse_stack(snippet)
+        resource_defns = self.stack.t.resource_definitions(self.stack)
+        self.rsrc_defn = resource_defns['GnoAggregationByMetricsAlarm']
+        self.client = mock.Mock()
+        self.patchobject(gnocchi.AodhGnocchiAggregationByMetricsAlarm,
+                         'client', return_value=self.client)
+
+        alarm_res = gnocchi.AodhGnocchiAggregationByMetricsAlarm(
+            'alarm', self.rsrc_defn, self.stack)
+        alarm_res.create()
+        value = {
+            'description': 'Do stuff with gnocchi metrics',
+            'alarm_actions': [],
+            'time_constraints': [],
+            'gnocchi_aggregation_by_metrics_threshold_rule': {
+                'metrics': ['911fce07-e0d7-4210-8c8c-4a9d811fcabc',
+                            '2543d435-fe93-4443-9351-fb0156930f94'],
+                'evaluation_periods': 1,
+                'aggregation_method': 'mean',
+                'granularity': 60,
+                'threshold': 50,
+                'comparison_operator': 'gt'
+            }
+        }
+        self.client.alarm.get.return_value = value
+        expected_data = {
+            'description': 'Do stuff with gnocchi metrics',
+            'alarm_actions': [],
+            'metrics': ['911fce07-e0d7-4210-8c8c-4a9d811fcabc',
+                        '2543d435-fe93-4443-9351-fb0156930f94'],
+            'evaluation_periods': 1,
+            'aggregation_method': 'mean',
+            'granularity': 60,
+            'threshold': 50,
+            'comparison_operator': 'gt',
+            'insufficient_data_actions': None,
+            'enabled': None,
+            'ok_actions': None,
+            'repeat_actions': None,
+            'severity': None
+        }
+        reality = alarm_res.get_live_state(alarm_res.properties)
+        self.assertEqual(expected_data, reality)
+
 
 class GnocchiAggregationByResourcesAlarmTest(GnocchiResourcesAlarmTest):
 
@@ -358,3 +453,52 @@ class GnocchiAggregationByResourcesAlarmTest(GnocchiResourcesAlarmTest):
         res.client().alarm.get.return_value = FakeAodhAlarm
         scheduler.TaskRunner(res.create)()
         self.assertEqual(FakeAodhAlarm, res.FnGetAtt('show'))
+
+    def test_gnocchi_alarm_aggr_by_resources_live_state(self):
+        snippet = template_format.parse(
+            gnocchi_aggregation_by_resources_alarm_template)
+        self.stack = utils.parse_stack(snippet)
+        resource_defns = self.stack.t.resource_definitions(self.stack)
+        self.rsrc_defn = resource_defns['GnoAggregationByResourcesAlarm']
+        self.client = mock.Mock()
+        self.patchobject(gnocchi.AodhGnocchiAggregationByResourcesAlarm,
+                         'client', return_value=self.client)
+
+        alarm_res = gnocchi.AodhGnocchiAggregationByResourcesAlarm(
+            'alarm', self.rsrc_defn, self.stack)
+        alarm_res.create()
+        value = {
+            'description': 'Do stuff with gnocchi aggregation by resource',
+            'alarm_actions': [],
+            'time_constraints': [],
+            'gnocchi_aggregation_by_resources_threshold_rule': {
+                'metric': 'cpu_util',
+                'resource_type': 'instance',
+                'query': "{'=': {'server_group': 'my_autoscaling_group'}}",
+                'evaluation_periods': 1,
+                'aggregation_method': 'mean',
+                'granularity': 60,
+                'threshold': 50,
+                'comparison_operator': 'gt'
+            }
+        }
+        self.client.alarm.get.return_value = value
+        expected_data = {
+            'description': 'Do stuff with gnocchi aggregation by resource',
+            'alarm_actions': [],
+            'metric': 'cpu_util',
+            'resource_type': 'instance',
+            'query': "{'=': {'server_group': 'my_autoscaling_group'}}",
+            'evaluation_periods': 1,
+            'aggregation_method': 'mean',
+            'granularity': 60,
+            'threshold': 50,
+            'comparison_operator': 'gt',
+            'insufficient_data_actions': None,
+            'enabled': None,
+            'ok_actions': None,
+            'repeat_actions': None,
+            'severity': None
+        }
+        reality = alarm_res.get_live_state(alarm_res.properties)
+        self.assertEqual(expected_data, reality)
