dd9ede99bc6e79a4b86a356dd8dd62ed051f3d57
Zane Bitter <zbitter@redhat.com>
2016-08-02 17:48:25 -0400

    Validate the input/output configs in Software Config
    When the only way to define a Software Config was via a Heat resource, the
    input and output configs were validated by the properties of the resource.
    However, subsequently a REST API to create Software Configs directly was
    added. That means that configs created in this way do not have the contents
    of the inputs and outputs sections validated. This change adds validation
    to ensure that the configs always follow the correct schema.
    
    Change-Id: I8c66bb82484b75723524959be753a4cd20c0f84d

---
+++ heat/engine/software_config_io.py
@@ -0,0 +1,164 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+APIs for dealing with input and output definitions for Software Configurations.
+"""
+
+import collections
+import six
+
+from heat.common.i18n import _
+
+from heat.common import exception
+from heat.engine import constraints
+from heat.engine import properties
+
+
+(
+    IO_NAME, DESCRIPTION, TYPE,
+    DEFAULT, VALUE,
+    ERROR_OUTPUT,
+) = (
+    'name', 'description', 'type',
+    'default', 'value',
+    'error_output',
+)
+
+TYPES = (
+    STRING_TYPE, NUMBER_TYPE, LIST_TYPE, JSON_TYPE, BOOLEAN_TYPE,
+) = (
+    'String', 'Number', 'CommaDelimitedList', 'Json', 'Boolean',
+)
+
+
+input_config_schema = {
+    IO_NAME: properties.Schema(
+        properties.Schema.STRING,
+        _('Name of the input.'),
+        required=True
+    ),
+    DESCRIPTION: properties.Schema(
+        properties.Schema.STRING,
+        _('Description of the input.')
+    ),
+    TYPE: properties.Schema(
+        properties.Schema.STRING,
+        _('Type of the value of the input.'),
+        default=STRING_TYPE,
+        constraints=[constraints.AllowedValues(TYPES)]
+    ),
+    DEFAULT: properties.Schema(
+        properties.Schema.STRING,
+        _('Default value for the input if none is specified.'),
+    ),
+}
+
+output_config_schema = {
+    IO_NAME: properties.Schema(
+        properties.Schema.STRING,
+        _('Name of the output.'),
+        required=True
+    ),
+    DESCRIPTION: properties.Schema(
+        properties.Schema.STRING,
+        _('Description of the output.')
+    ),
+    TYPE: properties.Schema(
+        properties.Schema.STRING,
+        _('Type of the value of the output.'),
+        default=STRING_TYPE,
+        constraints=[constraints.AllowedValues(TYPES)]
+    ),
+    ERROR_OUTPUT: properties.Schema(
+        properties.Schema.BOOLEAN,
+        _('Denotes that the deployment is in an error state if this '
+          'output has a value.'),
+        default=False
+    )
+}
+
+
+class IOConfig(object):
+    """Base class for the configuration data for a single input or output."""
+    def __init__(self, **config):
+        self._props = properties.Properties(self.schema, config)
+        try:
+            self._props.validate()
+        except exception.StackValidationFailed as exc:
+            raise ValueError(six.text_type(exc))
+
+    def name(self):
+        """Return the name of the input or output."""
+        return self._props[IO_NAME]
+
+    def as_dict(self):
+        """Return a dict representation suitable for persisting."""
+        return {k: v for k, v in self._props.items() if v is not None}
+
+    def __repr__(self):
+        return '%s(%s)' % (type(self).__name__,
+                           ', '.join('%s=%s' % (k, repr(v))
+                                     for k, v in self.as_dict().items()))
+
+
+_no_value = object()
+
+
+class InputConfig(IOConfig):
+    """Class representing the configuration data for a single input."""
+    schema = input_config_schema
+
+    def __init__(self, value=_no_value, **config):
+        super(InputConfig, self).__init__(**config)
+        self._value = value
+
+    def default(self):
+        """Return the default value of the input."""
+        return self._props[DEFAULT]
+
+    def as_dict(self):
+        """Return a dict representation suitable for persisting."""
+        d = super(InputConfig, self).as_dict()
+        if self._value is not _no_value:
+            d[VALUE] = self._value
+        return d
+
+    def input_data(self):
+        """Return a name, value pair for the input."""
+        value = self._value if self._value is not _no_value else None
+        return self.name(), value
+
+
+class OutputConfig(IOConfig):
+    """Class representing the configuration data for a single output."""
+    schema = output_config_schema
+
+    def error_output(self):
+        """Return True if the presence of the output indicates an error."""
+        return self._props[ERROR_OUTPUT]
+
+
+def check_io_schema_list(io_configs):
+    """Check that an input or output schema list is of the correct type.
+
+    Raises TypeError if the list itself is not a list, or if any of the
+    members are not dicts.
+    """
+    if (not isinstance(io_configs, collections.Sequence) or
+            isinstance(io_configs, collections.Mapping) or
+            isinstance(io_configs, six.string_types)):
+        raise TypeError('Software Config I/O Schema must be in a list')
+
+    if not all(isinstance(conf, collections.Mapping) for conf in io_configs):
+        raise TypeError('Software Config I/O Schema must be a dict')
--- heat/tests/engine/service/test_software_config.py
+++ heat/tests/engine/service/test_software_config.py
@@ -28,6 +28,7 @@ from heat.engine.clients.os import swift
 from heat.engine.clients.os import zaqar
 from heat.engine import service
 from heat.engine import service_software_config
+from heat.engine import software_config_io as swc_io
 from heat.objects import resource as resource_objects
 from heat.objects import software_config as software_config_object
 from heat.objects import software_deployment as software_deployment_object
@@ -148,8 +149,11 @@ class SoftwareConfigServiceTest(common.HeatTestCase):
         self.assertEqual(kwargs['group'], config['group'])
         self.assertEqual(kwargs['name'], config['name'])
         self.assertEqual(kwargs['config'], config['config'])
-        self.assertEqual(kwargs['inputs'], config['inputs'])
-        self.assertEqual(kwargs['outputs'], config['outputs'])
+        self.assertEqual([{'name': 'mode', 'type': 'String'}],
+                         config['inputs'])
+        self.assertEqual([{'name': 'endpoint', 'type': 'String',
+                           'error_output': False}],
+                         config['outputs'])
         self.assertEqual(kwargs['options'], config['options'])
 
     def test_delete_software_config(self):
@@ -910,3 +914,112 @@ class SoftwareConfigServiceTest(common.HeatTestCase):
         zaqar_client.queue.assert_called_once_with('6789')
         queue.pop.assert_called_once_with()
         ssd.assert_called_once_with(self.ctx, deployment_id, 'ok', None)
+
+
+class SoftwareConfigIOSchemaTest(common.HeatTestCase):
+    def test_input_config_empty(self):
+        name = 'foo'
+        inp = swc_io.InputConfig(name=name)
+        self.assertIsNone(inp.default())
+        self.assertEqual(name, inp.name())
+        self.assertEqual({'name': name, 'type': 'String'}, inp.as_dict())
+        self.assertEqual((name, None), inp.input_data())
+
+    def test_input_config(self):
+        name = 'bar'
+        inp = swc_io.InputConfig(name=name, description='test', type='Number',
+                                 default=0)
+        self.assertEqual('0', inp.default())
+        self.assertEqual(name, inp.name())
+        self.assertEqual({'name': name, 'type': 'Number',
+                          'description': 'test', 'default': '0'},
+                         inp.as_dict())
+        self.assertEqual((name, None), inp.input_data())
+
+    def test_input_config_value(self):
+        name = 'baz'
+        inp = swc_io.InputConfig(name=name, type='Number',
+                                 default=0, value=42)
+        self.assertEqual('0', inp.default())
+        self.assertEqual(name, inp.name())
+        self.assertEqual({'name': name, 'type': 'Number',
+                          'default': '0', 'value': 42},
+                         inp.as_dict())
+        self.assertEqual((name, 42), inp.input_data())
+
+    def test_input_config_no_name(self):
+        self.assertRaises(ValueError, swc_io.InputConfig, type='String')
+
+    def test_input_config_extra_key(self):
+        self.assertRaises(ValueError, swc_io.InputConfig,
+                          name='test', bogus='wat')
+
+    def test_input_types(self):
+        swc_io.InputConfig(name='str', type='String').as_dict()
+        swc_io.InputConfig(name='num', type='Number').as_dict()
+        swc_io.InputConfig(name='list', type='CommaDelimitedList').as_dict()
+        swc_io.InputConfig(name='json', type='Json').as_dict()
+        swc_io.InputConfig(name='bool', type='Boolean').as_dict()
+
+        self.assertRaises(ValueError, swc_io.InputConfig,
+                          name='bogus', type='BogusType')
+
+    def test_output_config_empty(self):
+        name = 'foo'
+        outp = swc_io.OutputConfig(name=name)
+        self.assertEqual(name, outp.name())
+        self.assertEqual({'name': name, 'type': 'String',
+                          'error_output': False},
+                         outp.as_dict())
+
+    def test_output_config(self):
+        name = 'bar'
+        outp = swc_io.OutputConfig(name=name, description='test',
+                                   type='Json', error_output=True)
+        self.assertEqual(name, outp.name())
+        self.assertIs(True, outp.error_output())
+        self.assertEqual({'name': name, 'type': 'Json',
+                          'description': 'test', 'error_output': True},
+                         outp.as_dict())
+
+    def test_output_config_no_name(self):
+        self.assertRaises(ValueError, swc_io.OutputConfig, type='String')
+
+    def test_output_config_extra_key(self):
+        self.assertRaises(ValueError, swc_io.OutputConfig,
+                          name='test', bogus='wat')
+
+    def test_output_types(self):
+        swc_io.OutputConfig(name='str', type='String').as_dict()
+        swc_io.OutputConfig(name='num', type='Number').as_dict()
+        swc_io.OutputConfig(name='list', type='CommaDelimitedList').as_dict()
+        swc_io.OutputConfig(name='json', type='Json').as_dict()
+        swc_io.OutputConfig(name='bool', type='Boolean').as_dict()
+
+        self.assertRaises(ValueError, swc_io.OutputConfig,
+                          name='bogus', type='BogusType')
+
+    def test_check_io_schema_empty_list(self):
+        swc_io.check_io_schema_list([])
+
+    def test_check_io_schema_string(self):
+        self.assertRaises(TypeError, swc_io.check_io_schema_list, '')
+
+    def test_check_io_schema_dict(self):
+        self.assertRaises(TypeError, swc_io.check_io_schema_list, {})
+
+    def test_check_io_schema_list_dict(self):
+        swc_io.check_io_schema_list([{'name': 'foo'}])
+
+    def test_check_io_schema_list_string(self):
+        self.assertRaises(TypeError, swc_io.check_io_schema_list, ['foo'])
+
+    def test_check_io_schema_list_list(self):
+        self.assertRaises(TypeError, swc_io.check_io_schema_list, [['foo']])
+
+    def test_check_io_schema_list_none(self):
+        self.assertRaises(TypeError, swc_io.check_io_schema_list, [None])
+
+    def test_check_io_schema_list_mixed(self):
+        self.assertRaises(TypeError, swc_io.check_io_schema_list,
+                          [{'name': 'foo'}, ('name', 'bar')])
