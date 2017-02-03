94565e709756b4bcc0a453ec71ef49bf887e1aae
Zane Bitter <zbitter@redhat.com>
2017-02-03 14:22:21 -0500

    Enable debug logging in unit tests
    When unit tests fail, you want to debug them 100% of the time. It's a giant
    pain not having DEBUG-level logs available. Even production deployments
    usually have debug enabled. The increase in the subunit file size is <10%.
    
    Change-Id: If235947a9896ab6a11259f5ce620b00b9e967b62

--- heat/tests/common.py
+++ heat/tests/common.py
@@ -46,16 +46,16 @@ TEST_DEFAULT_LOGLEVELS = {'migrate': logging.WARN,
                           'sqlalchemy': logging.WARN,
                           'heat.engine.environment': logging.ERROR}
 _LOG_FORMAT = "%(levelname)8s [%(name)s] %(message)s"
-_TRUE_VALUES = ('True', 'true', '1', 'yes')
+_FALSE_VALUES = {'False', 'false', '0', 'no'}
 
 
 class FakeLogMixin(object):
     def setup_logging(self, quieten=True):
         # Assign default logs to self.LOG so we can still
         # assert on heat logs.
-        default_level = logging.INFO
-        if os.environ.get('OS_DEBUG') in _TRUE_VALUES:
-            default_level = logging.DEBUG
+        default_level = logging.DEBUG
+        if os.environ.get('OS_DEBUG') in _FALSE_VALUES:
+            default_level = logging.INFO
 
         self.LOG = self.useFixture(
             fixtures.FakeLogger(level=default_level, format=_LOG_FORMAT))
