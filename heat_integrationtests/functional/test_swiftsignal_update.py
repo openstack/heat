9398dedd9871d95265d7e5081c14d969fb92aa37
Thomas Herve <therve@redhat.com>
2017-03-23 11:20:14 +0100

    Skip integration tests when Swift is not available
    Let's skip integration tests using Swift when the service is not
    available. It will help turn the py3 gate green.
    
    Change-Id: Ib3536583dbb3bbf50d9c64976e909fd352391d05

--- heat_integrationtests/functional/test_swiftsignal_update.py
+++ heat_integrationtests/functional/test_swiftsignal_update.py
@@ -34,6 +34,8 @@ outputs:
 class SwiftSignalHandleUpdateTest(functional_base.FunctionalTestsBase):
 
     def test_stack_update_same_template_replace_no_url(self):
+        if not self.is_service_available('object-store'):
+            self.skipTest('object-store service not available, skipping')
         stack_identifier = self.stack_create(template=test_template)
         stack = self.client.stacks.get(stack_identifier)
         orig_url = self._stack_output(stack, 'signal_url')
