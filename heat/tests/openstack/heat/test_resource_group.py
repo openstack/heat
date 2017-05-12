655fd8f53d4bf416c99114eacde12beed1f5cc3f
rabi <ramishra@redhat.com>
2017-05-12 19:20:17 +0530

    Use non-blacklisted resources for RG validation
    We should validate first of the non-blacklisted resources
    in the group.
    
    Change-Id: I1d15cb238125aff6c211f84b16da105d91fb4127
    Closes-Bug: #1690369

--- heat/tests/openstack/heat/test_resource_group.py
+++ heat/tests/openstack/heat/test_resource_group.py
@@ -19,11 +19,12 @@ import six
 from heat.common import exception
 from heat.common import grouputils
 from heat.common import template_format
+from heat.engine.clients.os import glance
+from heat.engine.clients.os import nova
 from heat.engine import node_data
 from heat.engine.resources.openstack.heat import resource_group
 from heat.engine import rsrc_defn
 from heat.engine import scheduler
-from heat.engine import stack as stackm
 from heat.tests import common
 from heat.tests import utils
 
@@ -114,12 +115,30 @@ template_attr = {
     }
 }
 
+template_server = {
+    "heat_template_version": "2013-05-23",
+    "resources": {
+        "group1": {
+            "type": "OS::Heat::ResourceGroup",
+            "properties": {
+                "count": 2,
+                "resource_def": {
+                    "type": "OS::Nova::Server",
+                    "properties": {
+                        "image": "image%index%",
+                        "flavor": "flavor%index%"
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 class ResourceGroupTest(common.HeatTestCase):
 
     def setUp(self):
         common.HeatTestCase.setUp(self)
-        self.m.StubOutWithMock(stackm.Stack, 'validate')
 
     def test_assemble_nested(self):
         """Tests nested stack creation based on props.
@@ -441,6 +460,30 @@ class ResourceGroupTest(common.HeatTestCase):
         templ = copy.deepcopy(template)
         res_def = templ["resources"]["group1"]["properties"]['resource_def']
         del res_def['properties']
+        stack = utils.parse_stack(templ)
+        resg = stack.resources['group1']
+        self.assertIsNone(resg.validate())
+
+    def test_validate_with_blacklist(self):
+        templ = copy.deepcopy(template_server)
+        self.mock_flavor = mock.Mock(ram=4, disk=4)
+        self.mock_active_image = mock.Mock(min_ram=1, min_disk=1,
+                                           status='active')
+        self.mock_inactive_image = mock.Mock(min_ram=1, min_disk=1,
+                                             status='inactive')
+
+        def get_image(image_identifier):
+            if image_identifier == 'image0':
+                return self.mock_inactive_image
+            else:
+                return self.mock_active_image
+
+        self.patchobject(glance.GlanceClientPlugin, 'get_image',
+                         side_effect=get_image)
+        self.patchobject(nova.NovaClientPlugin, 'get_flavor',
+                         return_value=self.mock_flavor)
+        props = templ["resources"]["group1"]["properties"]
+        props["removal_policies"] = [{"resource_list": ["0"]}]
         stack = utils.parse_stack(templ)
         resg = stack.resources['group1']
         self.assertIsNone(resg.validate())
