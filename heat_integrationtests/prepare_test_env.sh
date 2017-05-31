c8e1e82bd6216222f31bb959269ce23bdd65d3d5
Thomas Herve <therve@redhat.com>
2017-05-31 11:25:06 +0200

    Skip snapshot integration tests
    Change-Id: Ica5f87614f5fb98f253d65cbd4355dcc7c6714eb
    Related-Bug: #1694371

--- heat_integrationtests/prepare_test_env.sh
+++ heat_integrationtests/prepare_test_env.sh
@@ -70,6 +70,7 @@ iniset $conf_file heat_plugin skip_functional_test_list 'CancelUpdateTest.test_c
 # Add scenario tests to skip
 # VolumeBackupRestoreIntegrationTest skipped until failure rate can be reduced ref bug #1382300
 # test_server_signal_userdata_format_software_config is skipped untill bug #1651768 is resolved
-iniset $conf_file heat_plugin skip_scenario_test_list 'SoftwareConfigIntegrationTest, VolumeBackupRestoreIntegrationTest'
+# StackSnapshotRestoreTest skipped until bug #1694371 is resolved
+iniset $conf_file heat_plugin skip_scenario_test_list 'SoftwareConfigIntegrationTest, VolumeBackupRestoreIntegrationTest, StackSnapshotRestoreTest'
 
 cat $conf_file
