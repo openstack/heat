7b56e0e008c81d72adfa70187d9404ec93a7867c
ricolin <rico.lin@easystack.cn>
2017-10-11 23:44:09 +0800

    Remove OS::Heat::HARestarter
    The OS::Heat::HARestarter has been deprecated since Kilo. Now is the
    time to eliminate support and hide it from the documentation.
    
    This replaces it with a placeholder resource (like OS::Heat::None) and
    marks it as hidden.
    
    Change-Id: I56cd1f2d0b3323399ef02c3a0a05d79cc69af956

--- heat/engine/resources/openstack/heat/ha_restarter.py
+++ heat/engine/resources/openstack/heat/ha_restarter.py
@@ -12,101 +12,38 @@
 #    under the License.
 
 from oslo_log import log as logging
-import six
 
 from heat.common.i18n import _
-from heat.engine import attributes
-from heat.engine import constraints
-from heat.engine import properties
-from heat.engine.resources import signal_responder
+from heat.engine.resources.openstack.heat import none_resource
 from heat.engine import support
 
 LOG = logging.getLogger(__name__)
 
 
-class Restarter(signal_responder.SignalResponder):
+class Restarter(none_resource.NoneResource):
 
     support_status = support.SupportStatus(
-        support.DEPRECATED,
-        _('The HARestarter resource type is deprecated and will be removed '
-          'in a future release of Heat, once it has support for auto-healing '
-          'any type of resource. Note that HARestarter does *not* actually '
-          'restart servers - it deletes and then recreates them. It also does '
-          'the same to all dependent resources, and may therefore exhibit '
-          'unexpected and undesirable behaviour. Instead, use the '
-          'mark-unhealthy API to mark a resource as needing replacement, and '
-          'then a stack update to perform the replacement while respecting  '
-          'the dependencies and not deleting them unnecessarily.'),
-        version='2015.1'
-    )
-
-    PROPERTIES = (
-        INSTANCE_ID,
-    ) = (
-        'InstanceId',
-    )
-
-    ATTRIBUTES = (
-        ALARM_URL,
-    ) = (
-        'AlarmUrl',
-    )
-
-    properties_schema = {
-        INSTANCE_ID: properties.Schema(
-            properties.Schema.STRING,
-            _('Instance ID to be restarted.'),
-            required=True,
-            constraints=[
-                constraints.CustomConstraint('nova.server')
-            ]
-        ),
-    }
-
-    attributes_schema = {
-        ALARM_URL: attributes.Schema(
-            _("A signed url to handle the alarm (Heat extension)."),
-            type=attributes.Schema.STRING
-        ),
-    }
-
-    def handle_create(self):
-        super(Restarter, self).handle_create()
-        self.resource_id_set(self._get_user_id())
-
-    def handle_signal(self, details=None):
-        if details is None:
-            alarm_state = 'alarm'
-        else:
-            alarm_state = details.get('state', 'alarm').lower()
-
-        LOG.info('%(name)s Alarm, new state %(state)s',
-                 {'name': self.name, 'state': alarm_state})
-
-        if alarm_state != 'alarm':
-            return
-
-        target_id = self.properties[self.INSTANCE_ID]
-        victim = self.stack.resource_by_refid(target_id)
-        if victim is None:
-            LOG.info('%(name)s Alarm, can not find instance '
-                     '%(instance)s',
-                     {'name': self.name,
-                      'instance': target_id})
-            return
-
-        LOG.info('%(name)s Alarm, restarting resource: %(victim)s',
-                 {'name': self.name, 'victim': victim.name})
-        self.stack.restart_resource(victim.name)
-
-    def _resolve_attribute(self, name):
-        """Resolves the resource's attributes.
-
-        Heat extension: "AlarmUrl" returns the url to post to the policy
-        when there is an alarm.
-        """
-        if name == self.ALARM_URL and self.resource_id is not None:
-            return six.text_type(self._get_ec2_signed_url())
+        status=support.HIDDEN,
+        version='10.0.0',
+        message=_('The HARestarter resource type has been removed. Existing '
+                  'stacks containing HARestarter resources can still be '
+                  'used, but the HARestarter resource will be a placeholder '
+                  'that does nothing.'),
+        previous_status=support.SupportStatus(
+            status=support.DEPRECATED,
+            message=_('The HARestarter resource type is deprecated and will '
+                      'be removed in a future release of Heat, once it has '
+                      'support for auto-healing any type of resource. Note '
+                      'that HARestarter does *not* actually restart '
+                      'servers - it deletes and then recreates them. It also '
+                      'does the same to all dependent resources, and may '
+                      'therefore exhibit unexpected and undesirable '
+                      'behaviour. Instead, use the mark-unhealthy API to '
+                      'mark a resource as needing replacement, and then a '
+                      'stack update to perform the replacement while '
+                      'respecting  the dependencies and not deleting them '
+                      'unnecessarily.'),
+            version='2015.1'))
 
 
 def resource_mapping():
