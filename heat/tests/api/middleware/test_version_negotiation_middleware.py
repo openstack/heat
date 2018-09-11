fafdd06bfd8b4f0ee3005f6633acb3d61e2c6366
Benoît Knecht <benoit.knecht@fsfe.org>
2018-09-11 11:18:53 +0200

    middleware: return HTTPBadRequest when path is invalid
    Gracefully handle paths that are invalid UTF-8 by returning HTTP error
    400 instead of an Internal Server Error.
    
    Change-Id: I3b30e4d64e758eefd85f7a70fc645db69991b3d7
    Task: 26379

--- heat/tests/api/middleware/test_version_negotiation_middleware.py
+++ heat/tests/api/middleware/test_version_negotiation_middleware.py
@@ -136,3 +136,12 @@ class VersionNegotiationMiddlewareTest(common.HeatTestCase):
         response = version_negotiation.process_request(request)
 
         self.assertIsInstance(response, webob.exc.HTTPNotFound)
+
+    def test_invalid_utf8_path(self):
+        version_negotiation = vn.VersionNegotiationFilter(
+            self._version_controller_factory, None, None)
+        request = webob.Request.blank('/%c0')
+
+        response = version_negotiation.process_request(request)
+
+        self.assertIsInstance(response, webob.exc.HTTPBadRequest)
