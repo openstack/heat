32510405788c57eb0ffeededd927239dcce3cd81
Zane Bitter <zbitter@redhat.com>
2019-03-26 11:16:57 -0400

    Load existing resources using correct environment
    In convergence we were loading resources from the database using the
    current environment. This is incorrect when a previous update has
    failed, meaning the resources in the database were created with a
    non-current template and environment. If an attempt was made to change
    the type of a resource but that resource was never updated, this will
    result in us loading a resource with the wrong type. If the type has
    been removed then it can result in errors just trying to show the stack.
    
    Note that the Resource.load() method used during a convergence traversal
    already does the Right Thing - it only uses the new type if it is a
    valid substitution for the old type, and UpdateReplace is later raised
    in Resource.update_convergence() if the type does not match in that
    specified in the new environment. So we don't see any problems with
    stack updates, just with API calls.
    
    Since we cannot change the signature of Resource.__new__() without also
    modifying the signature of __init__() in every resource plugin that has
    implemented it (many of which are out of tree), instead substitute the
    stack definition for the duration of creating the Resource object. This
    will result in stack.env returning the environment the resource was last
    updated with.
    
    Change-Id: I3fbd14324fc4681b26747ee7505000b8fc9439f1
    Story: #2005090
    Task: 29688
    (cherry picked from commit aa58fbcacfdc14bb2e8d5a4ff1ef7a47fe9268d0)

--- heat_integrationtests/functional/test_create_update.py
+++ heat_integrationtests/functional/test_create_update.py
@@ -766,3 +766,37 @@ resources:
                           expected_status='UPDATE_FAILED')
         self.update_stack(stack_identifier, template=template,
                           expected_status='UPDATE_COMPLETE')
+
+    @test.requires_convergence
+    def test_update_failed_changed_env_list_resources(self):
+        template = {
+            'heat_template_version': 'rocky',
+            'resources': {
+                'test1': {
+                    'type': 'OS::Heat::TestResource',
+                    'properties': {
+                        'value': 'foo'
+                    }
+                },
+                'my_res': {
+                    'type': 'My::TestResource',
+                    'depends_on': 'test1'
+                },
+                'test2': {
+                    'depends_on': 'my_res',
+                    'type': 'OS::Heat::TestResource'
+                }
+            }
+        }
+        env = {'resource_registry':
+               {'My::TestResource': 'OS::Heat::TestResource'}}
+        stack_identifier = self.stack_create(
+            template=template, environment=env)
+        update_template = copy.deepcopy(template)
+        update_template['resources']['test1']['properties']['fail'] = 'true'
+        update_template['resources']['test2']['depends_on'] = 'test1'
+        del update_template['resources']['my_res']
+        self.update_stack(stack_identifier,
+                          template=update_template,
+                          expected_status='UPDATE_FAILED')
+        self.assertEqual(3, len(self.list_resources(stack_identifier)))
