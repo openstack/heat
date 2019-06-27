openstack/heat

commit d37cc18b1b7a531edbcf9658600fc467fbe24e2d

Refactor handle_update to use prop_diff
Changes remaining resources(Router, NetworkGateway) to
use prop_diff in handle_update.

Also refactors some common stuff to prepare_update_properties.

Change-Id: I18f4ce78492779261b542ff6de8898eb729947b8
Closes-Bug: #1521836

heat/engine/resources/openstack/neutron/firewall.py
@@ -130,8 +130,7 @@ class Firewall(neutron.NeutronResource):
 
     def handle_update(self, json_snippet, tmpl_diff, prop_diff):
         if prop_diff:
-            if self.VALUE_SPECS in prop_diff:
-                self.merge_value_specs(prop_diff)
+            self.prepare_update_properties(prop_diff)
             self.client().update_firewall(
                 self.resource_id, {'firewall': prop_diff})
 

heat/engine/resources/openstack/neutron/net.py
@@ -170,6 +170,7 @@ class Net(neutron.NeutronResource):
 
     def handle_update(self, json_snippet, tmpl_diff, prop_diff):
         if prop_diff:
+            self.prepare_update_properties(prop_diff)
             if self.DHCP_AGENT_IDS in prop_diff:
                 dhcp_agent_ids = prop_diff.pop(self.DHCP_AGENT_IDS, [])
                 self._replace_dhcp_agents(dhcp_agent_ids)
@@ -178,12 +179,6 @@ class Net(neutron.NeutronResource):
                 prop_diff[
                     'qos_policy_id'] = self.client_plugin().get_qos_policy_id(
                     qos_policy) if qos_policy else None
-            if self.VALUE_SPECS in prop_diff:
-                self.merge_value_specs(prop_diff)
-            if (self.NAME in prop_diff and
-                    prop_diff[self.NAME] is None):
-                prop_diff[self.NAME] = self.physical_resource_name()
-
         if prop_diff:
             self.client().update_network(self.resource_id,
                                          {'network': prop_diff})

heat/engine/resources/openstack/neutron/network_gateway.py
@@ -223,22 +223,22 @@ class NetworkGateway(neutron.NeutronResource):
             return True
 
     def handle_update(self, json_snippet, tmpl_diff, prop_diff):
-        props = self.prepare_update_properties(json_snippet)
-        connections = props.pop(self.CONNECTIONS)
+        connections = None
+        if self.CONNECTIONS in prop_diff:
+            connections = prop_diff.pop(self.CONNECTIONS)
 
         if self.DEVICES in prop_diff:
             self.handle_delete()
-            self.properties.data.update(props)
+            self.properties.data.update(prop_diff)
             self.handle_create()
             return
-        else:
-            props.pop(self.DEVICES, None)
 
-        if self.NAME in prop_diff:
+        if prop_diff:
+            self.prepare_update_properties(prop_diff)
             self.client().update_network_gateway(
-                self.resource_id, {'network_gateway': props})
+                self.resource_id, {'network_gateway': prop_diff})
 
-        if self.CONNECTIONS in prop_diff:
+        if connections:
             for connection in self.properties[self.CONNECTIONS]:
                 with self.client_plugin().ignore_not_found:
                     self.client_plugin().resolve_network(

heat/engine/resources/openstack/neutron/neutron.py
@@ -75,24 +75,16 @@ class NeutronResource(resource.Resource):
         value_spec_props = props.pop('value_specs')
         props.update(value_spec_props)
 
-    def prepare_update_properties(self, definition):
-        """Prepares the property values for correct Neutron update call.
+    def prepare_update_properties(self, prop_diff):
+        """Prepares prop_diff values for correct neutron update call.
 
-        Prepares the property values so that they can be passed directly to
-        the Neutron update call.
-
-        Removes any properties which are not update_allowed, then processes
-        as for prepare_properties.
+        1. Merges value_specs
+        2. Defaults resource name to physical resource name if None
         """
-        p = definition.properties(self.properties_schema, self.context)
-        update_props = dict((k, v) for k, v in p.items()
-                            if p.props.get(k).schema.update_allowed and
-                            k not in self.update_exclude_properties)
-
-        props = self.prepare_properties(
-            update_props,
-            self.physical_resource_name())
-        return props
+        if 'value_specs' in prop_diff and prop_diff['value_specs']:
+            NeutronResource.merge_value_specs(prop_diff)
+        if 'name' in prop_diff and prop_diff['name'] is None:
+            prop_diff['name'] = self.physical_resource_name()
 
     @staticmethod
     def is_built(attributes):

heat/engine/resources/openstack/neutron/port.py
@@ -475,15 +475,11 @@ class Port(neutron.NeutronResource):
 
     def handle_update(self, json_snippet, tmpl_diff, prop_diff):
         if prop_diff:
-            if self.VALUE_SPECS in prop_diff:
-                self.merge_value_specs(prop_diff)
+            self.prepare_update_properties(prop_diff)
             if self.QOS_POLICY in prop_diff:
                 qos_policy = prop_diff.pop(self.QOS_POLICY)
                 prop_diff['qos_policy_id'] = self.client_plugin(
                     ).get_qos_policy_id(qos_policy) if qos_policy else None
-            if (self.NAME in prop_diff and
-                    prop_diff[self.NAME] is None):
-                prop_diff[self.NAME] = self.physical_resource_name()
             self._prepare_port_properties(prop_diff, prepare_for_update=True)
             LOG.debug('updating port with %s' % prop_diff)
             self.client().update_port(self.resource_id, {'port': prop_diff})

heat/engine/resources/openstack/neutron/provider_net.py
@@ -138,9 +138,7 @@ class ProviderNet(net.Net):
         """
         if prop_diff:
             ProviderNet.prepare_provider_properties(prop_diff)
-            if (self.NAME in prop_diff and
-                    prop_diff[self.NAME] is None):
-                prop_diff[self.NAME] = self.physical_resource_name()
+            self.prepare_update_properties(prop_diff)
             self.client().update_network(self.resource_id,
                                          {'network': prop_diff})
 

heat/engine/resources/openstack/neutron/qos.py
@@ -94,10 +94,10 @@ class QoSPolicy(neutron.NeutronResource):
 
     def handle_update(self, json_snippet, tmpl_diff, prop_diff):
         if prop_diff:
-            props = self.prepare_update_properties(json_snippet)
+            self.prepare_update_properties(prop_diff)
             self.client().update_qos_policy(
                 self.resource_id,
-                {'policy': props})
+                {'policy': prop_diff})
 
     def _show_resource(self):
         return self.client().show_qos_policy(

heat/engine/resources/openstack/neutron/router.py
@@ -209,8 +209,7 @@ class Router(neutron.NeutronResource):
                     if subnet_net == external_gw_net:
                         deps += (self, res)
 
-    def prepare_properties(self, properties, name):
-        props = super(Router, self).prepare_properties(properties, name)
+    def _resolve_gateway(self, props):
         gateway = props.get(self.EXTERNAL_GATEWAY)
         if gateway:
             self.client_plugin().resolve_network(
@@ -231,7 +230,7 @@ class Router(neutron.NeutronResource):
         props = self.prepare_properties(
             self.properties,
             self.physical_resource_name())
-
+        self._resolve_gateway(props)
         l3_agent_ids = self._get_l3_agent_list(props)
 
         router = self.client().create_router({'router': props})['router']
@@ -257,19 +256,17 @@ class Router(neutron.NeutronResource):
             return True
 
     def handle_update(self, json_snippet, tmpl_diff, prop_diff):
-        props = self.prepare_update_properties(json_snippet)
-        l3_agent_ids = self._get_l3_agent_list(props)
-        if l3_agent_ids:
+        if self.EXTERNAL_GATEWAY in prop_diff:
+            self._resolve_gateway(prop_diff)
+
+        if self.L3_AGENT_IDS in prop_diff or self.L3_AGENT_ID in prop_diff:
+            l3_agent_ids = self._get_l3_agent_list(prop_diff)
             self._replace_agent(l3_agent_ids)
 
-        if self.L3_AGENT_IDS in prop_diff:
-            del prop_diff[self.L3_AGENT_IDS]
-        if self.L3_AGENT_ID in prop_diff:
-            del prop_diff[self.L3_AGENT_ID]
-
-        if len(prop_diff) > 0:
+        if prop_diff:
+            self.prepare_update_properties(prop_diff)
             self.client().update_router(
-                self.resource_id, {'router': props})
+                self.resource_id, {'router': prop_diff})
 
     def _replace_agent(self, l3_agent_ids=None):
         ret = self.client().list_l3_agent_hosting_routers(

commit dd9bc3cd29c5d0c27b3152a6e6aa5edb071c9391

Unit tests: Fix wrong assert function name in port update
Due to a typo in an assert function name (assset_called_once_with()
instead of assert_called_once_with()), we weren't actually checking the
arguments passed to Neutron in UpdatePortTest. Fix the function name and
modify the test so it actually works.

Among the fixes required:

- Actually return data from create_port
- Call Resource.create() instead of handle_create(), to ensure that the
  resource is stored in the DB (and hence that it gets a name)
- Mock show_port(), which is called from check_create_complete()
- Handle later changes to the format of data passed (e.g. an empty
  allowed_address_pairs should be passed as an empty list instead of None)
- Actually test the case where the port name is initially specified, and
  then removed on update
- Fix the format of arguments to create_port and update_port

This patch contains a workaround for a regression introduced by
d37cc18b1b7a531edbcf9658600fc467fbe24e2d that causes a 'value_specs' key
to be passed to the update_port call. This will be fixed in a subsequent
patch.

Change-Id: I37948721a0a653a18049aae6502fafb88030479c
Co-Authored-By: zhufl <zhu.fanglei@zte.com.cn>
Task: 34764

heat/tests/openstack/neutron/test_neutron_port.py
@@ -927,6 +927,7 @@ class UpdatePortTest(common.HeatTestCase):
                              addr_pair=None,
                              vnic_type=None)),
         ('with_no_name', dict(secgrp=['8a2f582a-e1cd-480f-b85d-b02631c10656'],
+                              orig_name='original',
                               name=None,
                               value_specs={},
                               fixed_ips=None,
@@ -990,12 +991,43 @@ class UpdatePortTest(common.HeatTestCase):
 
     def test_update_port(self):
         t = template_format.parse(neutron_port_template)
+        create_name = getattr(self, 'orig_name', None)
+        if create_name is not None:
+            t['resources']['port']['properties']['name'] = create_name
         stack = utils.parse_stack(t)
 
+        def res_id(client, resource, name_or_id, cmd_resource=None):
+            return {
+                'network': 'net1234',
+                'subnet': 'sub1234',
+            }[resource]
+
         self.patchobject(neutronV20, 'find_resourceid_by_name_or_id',
-                         return_value='net1234')
-        create_port = self.patchobject(neutronclient.Client, 'create_port')
+                         side_effect=res_id)
+
+        create_port_result = {
+            'port': {
+                "status": "BUILD",
+                "id": "fc68ea2c-b60b-4b4f-bd82-94ec81110766"
+            }
+        }
+        show_port_result = {
+            'port': {
+                "status": "ACTIVE",
+                "id": "fc68ea2c-b60b-4b4f-bd82-94ec81110766",
+                "fixed_ips": {
+                    "subnet_id": "d0e971a6-a6b4-4f4c-8c88-b75e9c120b7e",
+                    "ip_address": "10.0.0.2"
+                }
+            }
+        }
+
+        create_port = self.patchobject(neutronclient.Client, 'create_port',
+                                       return_value=create_port_result)
         update_port = self.patchobject(neutronclient.Client, 'update_port')
+        show_port = self.patchobject(neutronclient.Client, 'show_port',
+                                     return_value=show_port_result)
+
         fake_groups_list = {
             'security_groups': [
                 {
@@ -1012,11 +1044,17 @@ class UpdatePortTest(common.HeatTestCase):
         set_tag_mock = self.patchobject(neutronclient.Client, 'replace_tag')
 
         props = {'network_id': u'net1234',
-                 'name': str(utils.PhysName(stack.name, 'port')),
+                 'fixed_ips': [{'subnet_id': 'sub1234',
+                                'ip_address': '10.0.3.21'}],
+                 'name': (create_name if create_name is not None else
+                          utils.PhysName(stack.name, 'port')),
                  'admin_state_up': True,
-                 'device_owner': u'network:dhcp'}
+                 'device_owner': u'network:dhcp',
+                 'device_id': '',
+                 'binding:vnic_type': 'normal'}
 
         update_props = props.copy()
+        update_props['name'] = getattr(self, 'name', create_name)
         update_props['security_groups'] = self.secgrp
         update_props['value_specs'] = self.value_specs
         update_props['tags'] = ['test_tag']
@@ -1027,23 +1065,32 @@ class UpdatePortTest(common.HeatTestCase):
 
         update_dict = update_props.copy()
 
+        if update_props['allowed_address_pairs'] is None:
+            update_dict['allowed_address_pairs'] = []
+
         if update_props['security_groups'] is None:
-            update_dict['security_groups'] = ['default']
+            update_dict['security_groups'] = [
+                '0389f747-7785-4757-b7bb-2ab07e4b09c3',
+            ]
 
         if update_props['name'] is None:
-            update_dict['name'] = utils.PhysName(stack.name, 'test_subnet')
+            update_dict['name'] = utils.PhysName(stack.name, 'port')
 
         value_specs = update_dict.pop('value_specs')
         if value_specs:
             for value_spec in six.iteritems(value_specs):
                 update_dict[value_spec[0]] = value_spec[1]
+        else:
+            # BUG(zaneb): this is necessary because of a regression
+            # from https://review.opendev.org/263119
+            update_dict['value_specs'] = {}
 
         tags = update_dict.pop('tags')
 
         # create port
         port = stack['port']
-        self.assertIsNone(scheduler.TaskRunner(port.handle_create)())
-        create_port.assset_called_once_with(props)
+        self.assertIsNone(scheduler.TaskRunner(port.create)())
+        create_port.assert_called_once_with({'port': props})
         # update port
         update_snippet = rsrc_defn.ResourceDefinition(port.name, port.type(),
                                                       update_props)
@@ -1051,7 +1098,8 @@ class UpdatePortTest(common.HeatTestCase):
                                                update_snippet, {},
                                                update_props)())
 
-        update_port.assset_called_once_with(update_dict)
+        update_port.assert_called_once_with(
+            'fc68ea2c-b60b-4b4f-bd82-94ec81110766', {'port': update_dict})
         set_tag_mock.assert_called_with('ports', port.resource_id,
                                         {'tags': tags})
         # check, that update does not cause of Update Replace
@@ -1069,3 +1117,4 @@ class UpdatePortTest(common.HeatTestCase):
         # update with empty prop_diff
         scheduler.TaskRunner(port.handle_update, update_snippet, {}, {})()
         self.assertEqual(1, update_port.call_count)
+        show_port.assert_called_with('fc68ea2c-b60b-4b4f-bd82-94ec81110766')