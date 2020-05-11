f18a6767927965b930579995eb0b3d13c647bc26
Rabi Mishra <ramishra@redhat.com>
2020-05-11 17:16:38 +0530

    Use Fedora 31 for tests
    Change-Id: Ia45e957bb24f5596a5ef16271f71ba8b77df9e42

--- heat_integrationtests/cleanup_test_env.sh
+++ heat_integrationtests/cleanup_test_env.sh
@@ -30,4 +30,4 @@ openstack flavor delete m1.heat_int
 openstack flavor delete m1.heat_micro
 
 # delete the image created
-openstack image delete Fedora-Cloud-Base-30-1.2.x86_64
+openstack image delete Fedora-Cloud-Base-31-1.9.x86_64
