61e53c4eca3bc4f0c60754269cb2811afc569705
Matthias Runge <mrunge@redhat.com>
2020-12-03 14:07:05 +0100

    Replace Fedora test image with F32
    Fedora 31 was retired and the image is gone from mirrors.
    heat-cfntools have been dropped from fedora images, disable
    the test till that's resolved.
    
    Also makes grenade job non-voting, till this is backported
    to stable/victoria.
    
    Change-Id: Id869f83a46454897c2fe7a532eebfa2863befe5e

--- heat_integrationtests/cleanup_test_env.sh
+++ heat_integrationtests/cleanup_test_env.sh
@@ -30,4 +30,4 @@ openstack flavor delete m1.heat_int
 openstack flavor delete m1.heat_micro
 
 # delete the image created
-openstack image delete Fedora-Cloud-Base-31-1.9.x86_64
+openstack image delete Fedora-Cloud-Base-32-1.6.x86_64
