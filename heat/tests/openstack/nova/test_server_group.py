openstack/heat

commit 74164424016702bac6d52c6618360287ce7ea461

Fix ServerGroup create
Nova api has a different signature from microversion
2.64 onwards[1].

Also adds support_status for 'rules' property missed
earlier in I8e77f54303298da00cbe719afccb449f10fe387c.

[1] https://github.com/openstack/python-novaclient/blob/master/novaclient/v2/server_groups.py#L103-L104

Task: 43168
Task: 43661
Change-Id: If7138c71044adadaf7de255a2fac463b57ff3c47
(cherry picked from commit 9aedaa6d8ab2a799f140fd7c01ad30342d0cc2aa,
 e70ff2e1533412727f8aeb3e08ffd9a8cefef6f9)

heat/engine/resources/openstack/nova/server_group.py
@@ -35,10 +35,12 @@ class ServerGroup(resource.Resource):
     entity = 'server_groups'
 
     PROPERTIES = (
-        NAME, POLICIES, RULE
+        NAME, POLICIES, RULES
     ) = (
-        'name', 'policies', 'rule'
+        'name', 'policies', 'rules'
     )
+
+    _RULES = (MAX_SERVER_PER_HOST) = ('max_server_per_host')
 
     properties_schema = {
         NAME: properties.Schema(
@@ -47,7 +49,7 @@ class ServerGroup(resource.Resource):
         ),
         POLICIES: properties.Schema(
             properties.Schema.LIST,
-            _('A list of string policies to apply. '
+            _('A list of exactly one policy to apply. '
               'Defaults to anti-affinity.'),
             default=['anti-affinity'],
             constraints=[
@@ -59,11 +61,17 @@ class ServerGroup(resource.Resource):
                 properties.Schema.STRING,
             ),
         ),
-        RULE: properties.Schema(
+        RULES: properties.Schema(
             properties.Schema.MAP,
-            _('A rule for the policy. Currently, only the '
-              '"max_server_per_host" rule is supported for the '
-              '"anti-affinity" policy.'),
+            _('Rules for a policy.'),
+            schema={
+                MAX_SERVER_PER_HOST: properties.Schema(
+                    properties.Schema.NUMBER,
+                    _('Maximum servers in a group on a given host. '
+                      'Rule for anti-affinity policy.')
+                )
+            },
+            support_status=support.SupportStatus(version='17.0.0'),
         ),
     }
 
@@ -77,26 +85,25 @@ class ServerGroup(resource.Resource):
             msg = _('Required microversion for soft policies not supported.')
             raise exception.StackValidationFailed(message=msg)
 
-        if self.properties[self.RULE]:
+        if self.properties[self.RULES]:
             is_supported = self.client_plugin().is_version_supported(
                 MICROVERSION_RULE)
             if not is_supported:
-                msg = _('Required microversion for rule not supported.')
+                msg = _('Required microversion for rules not supported.')
                 raise exception.StackValidationFailed(message=msg)
 
     def handle_create(self):
         name = self.physical_resource_name()
         policies = self.properties[self.POLICIES]
-        if self.properties[self.RULE] and 'soft-affinity' in policies:
-            rule = self.properties[self.RULE]
-            client = self.client()
-            server_group = client.server_groups.create(name=name,
-                                                       policies=policies,
-                                                       rule=rule)
+        rules = self.properties[self.RULES]
+        rules_supported = self.client_plugin().is_version_supported(
+            MICROVERSION_RULE)
+        if rules_supported:
+            server_group = self.client().server_groups.create(
+                name=name, policy=policies[0], rules=rules)
         else:
-            client = self.client()
-            server_group = client.server_groups.create(name=name,
-                                                       policies=policies)
+            server_group = self.client().server_groups.create(
+                name=name, policies=policies)
         self.resource_id_set(server_group.id)
 
     def physical_resource_name(self):

heat/tests/openstack/nova/test_server_group.py
@@ -26,7 +26,9 @@ sg_template = {
             "type": "OS::Nova::ServerGroup",
             "properties": {
                 "name": "test",
-                "policies": ["anti-affinity"]
+                "policies": ["anti-affinity"],
+                "rules": {
+                    "max_server_per_host": 8}
             }
         }
     }
@@ -50,6 +52,12 @@ class NovaServerGroupTest(common.HeatTestCase):
         # create mock clients and objects
         nova = mock.MagicMock()
         self.sg.client = mock.MagicMock(return_value=nova)
+        mock_plugin = mock.MagicMock()
+        self.patchobject(mock_plugin,
+                         'is_version_supported',
+                         return_value=True)
+        self.patchobject(self.sg, 'client_plugin',
+                         return_value=mock_plugin)
         self.sg_mgr = nova.server_groups
 
     def _create_sg(self, name):
@@ -68,7 +76,7 @@ class NovaServerGroupTest(common.HeatTestCase):
             name = 'test'
             n = name
 
-            def fake_create(name, policies):
+            def fake_create(name, policy, rules):
                 self.assertGreater(len(name), 1)
                 return FakeGroup(n)
             self.sg_mgr.create = fake_create
@@ -81,7 +89,9 @@ class NovaServerGroupTest(common.HeatTestCase):
         self._create_sg('test')
         expected_args = ()
         expected_kwargs = {'name': 'test',
-                           'policies': ["anti-affinity"],
+                           'policy': "anti-affinity",
+                           'rules': {
+                               'max_server_per_host': 8}
                            }
         self.sg_mgr.create.assert_called_once_with(*expected_args,
                                                    **expected_kwargs)

commit a4f2f6053c8b45b1e9182caad4a8af44e29ae96d

Don't always replace FAILED ServerGroup resources
ServerGroup resources work as scheduler hints for server
resources and replacement of it replaces the servers which
is undesirable in many circumstances.

- We don't allow update of ServerGroup resource properties
- If not created (CREATE_FAILED) without resource_id they will
  be replaced
- If does not exist in nova they would be created
- If exist in nova, FAILED resource status would be just
  changed to COMPLETE

Conflicts:
	heat/tests/openstack/nova/test_server_group.py

Fixed conflicts caused by 74164424016702bac6d52c6618360287ce7ea461 ,
which is present in stable/xena and later.

Task: 45748

Change-Id: I096dff2b541a5aa8afbbbcea5161e7ca1c244039
(cherry picked from commit a0e072b3204d736b9a4a5b2ceb2157f57405ac6f)
(cherry picked from commit f9f5d2cc368e5f850b962c8d89669a50544058d1)
(cherry picked from commit ead7cd61bd685ea46dd21c4ef3ea6b3fde273c10)

heat/engine/resources/openstack/nova/server_group.py
@@ -78,6 +78,16 @@ class ServerGroup(resource.Resource):
                                                    policies=policies)
         self.resource_id_set(server_group.id)
 
+    def needs_replace_failed(self):
+        if not self.resource_id:
+            return True
+
+        with self.client_plugin().ignore_not_found:
+            self._show_resource()
+            return False
+
+        return True
+
     def physical_resource_name(self):
         name = self.properties[self.NAME]
         if name:

heat/tests/openstack/nova/test_server_group.py
@@ -14,6 +14,9 @@
 import json
 from unittest import mock
 
+from novaclient import exceptions
+from oslo_utils import excutils
+
 from heat.common import template_format
 from heat.engine import scheduler
 from heat.tests import common
@@ -50,6 +53,20 @@ class NovaServerGroupTest(common.HeatTestCase):
         # create mock clients and objects
         nova = mock.MagicMock()
         self.sg.client = mock.MagicMock(return_value=nova)
+
+        class FakeNovaPlugin(object):
+
+            @excutils.exception_filter
+            def ignore_not_found(self, ex):
+                if not isinstance(ex, exceptions.NotFound):
+                    raise ex
+
+            def is_conflict(self, ex):
+                return False
+
+        self.patchobject(excutils.exception_filter, '__exit__')
+        self.patchobject(self.sg, 'client_plugin',
+                         return_value=FakeNovaPlugin())
         self.sg_mgr = nova.server_groups
 
     def _create_sg(self, name):
@@ -99,3 +116,21 @@ class NovaServerGroupTest(common.HeatTestCase):
         self.sg.client().server_groups = s_groups
         self.assertEqual({'server_gr': 'info'}, self.sg.FnGetAtt('show'))
         s_groups.get.assert_called_once_with('test')
+
+    def test_needs_replace_failed(self):
+        self._create_sg('test')
+        self.sg.state_set(self.sg.CREATE, self.sg.FAILED)
+        mock_show_resource = self.patchobject(self.sg, '_show_resource')
+        mock_show_resource.side_effect = [exceptions.NotFound(404), None]
+
+        self.sg.resource_id = None
+        self.assertTrue(self.sg.needs_replace_failed())
+        self.assertEqual(0, mock_show_resource.call_count)
+
+        self.sg.resource_id = 'sg_id'
+        self.assertTrue(self.sg.needs_replace_failed())
+        self.assertEqual(1, mock_show_resource.call_count)
+
+        mock_show_resource.return_value = None
+        self.assertFalse(self.sg.needs_replace_failed())
+        self.assertEqual(2, mock_show_resource.call_count)