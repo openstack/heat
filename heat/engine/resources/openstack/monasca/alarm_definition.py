75075223653d9e0cab33aeddda2363128b83e38b
Takashi Kajinami <kajinamit@oss.nttdata.com>
2023-11-09 18:39:31 +0900

    Remove Monasca support
    The monasca project was marked inactive during 2023.1 cycle[1] and is
    being formally retired.
    
    [1] https://review.opendev.org/c/openstack/governance/+/897520
    
    Change-Id: I57f88f08bd0d160760703891b00aa9cb26dd1d98
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- heat/engine/resources/openstack/monasca/alarm_definition.py
+++ heat/engine/resources/openstack/monasca/alarm_definition.py
@@ -12,13 +12,11 @@
 #    under the License.
 
 from heat.common.i18n import _
-from heat.engine import constraints
-from heat.engine import properties
-from heat.engine import resource
+from heat.engine.resources.openstack.heat import none_resource
 from heat.engine import support
 
 
-class MonascaAlarmDefinition(resource.Resource):
+class MonascaAlarmDefinition(none_resource.NoneResource):
     """Heat Template Resource for Monasca Alarm definition.
 
     Monasca Alarm definition helps to define the required expression for
@@ -32,175 +30,19 @@ class MonascaAlarmDefinition(resource.Resource):
     """
 
     support_status = support.SupportStatus(
-        version='22.0.0',
-        status=support.DEPRECATED,
-        message=_('Monasca project was marked inactive'),
+        version='25.0.0',
+        status=support.HIDDEN,
+        message=_('Monasca project was retired'),
         previous_status=support.SupportStatus(
-            version='7.0.0',
+            version='22.0.0',
+            status=support.DEPRECATED,
+            message=_('Monasca project was marked inactive'),
             previous_status=support.SupportStatus(
-                version='5.0.0',
-                status=support.UNSUPPORTED
-            )))
-
-    default_client_name = 'monasca'
-
-    entity = 'alarm_definitions'
-
-    SEVERITY_LEVELS = (
-        LOW, MEDIUM, HIGH, CRITICAL
-    ) = (
-        'low', 'medium', 'high', 'critical'
-    )
-
-    PROPERTIES = (
-        NAME, DESCRIPTION, EXPRESSION, MATCH_BY, SEVERITY,
-        OK_ACTIONS, ALARM_ACTIONS, UNDETERMINED_ACTIONS,
-        ACTIONS_ENABLED
-    ) = (
-        'name', 'description', 'expression', 'match_by', 'severity',
-        'ok_actions', 'alarm_actions', 'undetermined_actions',
-        'actions_enabled'
-    )
-
-    properties_schema = {
-        NAME: properties.Schema(
-            properties.Schema.STRING,
-            _('Name of the alarm. By default, physical resource name is '
-              'used.'),
-            update_allowed=True
-        ),
-        DESCRIPTION: properties.Schema(
-            properties.Schema.STRING,
-            _('Description of the alarm.'),
-            update_allowed=True
-        ),
-        EXPRESSION: properties.Schema(
-            properties.Schema.STRING,
-            _('Expression of the alarm to evaluate.'),
-            update_allowed=False,
-            required=True
-        ),
-        MATCH_BY: properties.Schema(
-            properties.Schema.LIST,
-            _('The metric dimensions to match to the alarm dimensions. '
-              'One or more dimension key names separated by a comma.'),
-            default=[],
-        ),
-        SEVERITY: properties.Schema(
-            properties.Schema.STRING,
-            _('Severity of the alarm.'),
-            update_allowed=True,
-            constraints=[constraints.AllowedValues(
-                SEVERITY_LEVELS
-            )],
-            default=LOW
-        ),
-        OK_ACTIONS: properties.Schema(
-            properties.Schema.LIST,
-            _('The notification methods to use when an alarm state is OK.'),
-            update_allowed=True,
-            schema=properties.Schema(
-                properties.Schema.STRING,
-                _('Monasca notification.'),
-                constraints=[constraints.CustomConstraint(
-                    'monasca.notification')
-                ]
-            ),
-            default=[],
-        ),
-        ALARM_ACTIONS: properties.Schema(
-            properties.Schema.LIST,
-            _('The notification methods to use when an alarm state is ALARM.'),
-            update_allowed=True,
-            schema=properties.Schema(
-                properties.Schema.STRING,
-                _('Monasca notification.'),
-                constraints=[constraints.CustomConstraint(
-                    'monasca.notification')
-                ]
-            ),
-            default=[],
-        ),
-        UNDETERMINED_ACTIONS: properties.Schema(
-            properties.Schema.LIST,
-            _('The notification methods to use when an alarm state is '
-              'UNDETERMINED.'),
-            update_allowed=True,
-            schema=properties.Schema(
-                properties.Schema.STRING,
-                _('Monasca notification.'),
-                constraints=[constraints.CustomConstraint(
-                    'monasca.notification')
-                ]
-            ),
-            default=[],
-        ),
-        ACTIONS_ENABLED: properties.Schema(
-            properties.Schema.BOOLEAN,
-            _('Whether to enable the actions or not.'),
-            update_allowed=True,
-            default=True,
-        ),
-    }
-
-    def handle_create(self):
-        args = dict(
-            name=(self.properties[self.NAME] or
-                  self.physical_resource_name()),
-            description=self.properties[self.DESCRIPTION],
-            expression=self.properties[self.EXPRESSION],
-            match_by=self.properties[self.MATCH_BY],
-            severity=self.properties[self.SEVERITY],
-            ok_actions=self.properties[self.OK_ACTIONS],
-            alarm_actions=self.properties[self.ALARM_ACTIONS],
-            undetermined_actions=self.properties[
-                self.UNDETERMINED_ACTIONS]
-        )
-
-        alarm = self.client().alarm_definitions.create(**args)
-        self.resource_id_set(alarm['id'])
-
-        # Monasca enables action by default
-        actions_enabled = self.properties[self.ACTIONS_ENABLED]
-        if not actions_enabled:
-            self.client().alarm_definitions.patch(
-                alarm_id=self.resource_id,
-                actions_enabled=actions_enabled
-            )
-
-    def handle_update(self, json_snippet, tmpl_diff, prop_diff):
-        args = dict(alarm_id=self.resource_id)
-
-        if prop_diff.get(self.NAME):
-            args['name'] = prop_diff.get(self.NAME)
-
-        if prop_diff.get(self.DESCRIPTION):
-            args['description'] = prop_diff.get(self.DESCRIPTION)
-
-        if prop_diff.get(self.SEVERITY):
-            args['severity'] = prop_diff.get(self.SEVERITY)
-
-        if prop_diff.get(self.OK_ACTIONS):
-            args['ok_actions'] = prop_diff.get(self.OK_ACTIONS)
-
-        if prop_diff.get(self.ALARM_ACTIONS):
-            args['alarm_actions'] = prop_diff.get(self.ALARM_ACTIONS)
-
-        if prop_diff.get(self.UNDETERMINED_ACTIONS):
-            args['undetermined_actions'] = prop_diff.get(
-                self.UNDETERMINED_ACTIONS
-            )
-
-        if prop_diff.get(self.ACTIONS_ENABLED):
-            args['actions_enabled'] = prop_diff.get(self.ACTIONS_ENABLED)
-
-        self.client().alarm_definitions.patch(**args)
-
-    def handle_delete(self):
-        if self.resource_id is not None:
-            with self.client_plugin().ignore_not_found:
-                self.client().alarm_definitions.delete(
-                    alarm_id=self.resource_id)
+                version='7.0.0',
+                previous_status=support.SupportStatus(
+                    version='5.0.0',
+                    status=support.UNSUPPORTED
+                ))))
 
 
 def resource_mapping():
--- heat/engine/resources/openstack/monasca/notification.py
+++ heat/engine/resources/openstack/monasca/notification.py
@@ -11,18 +11,12 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import re
-from urllib import parse
-
-from heat.common import exception
 from heat.common.i18n import _
-from heat.engine import constraints
-from heat.engine import properties
-from heat.engine import resource
+from heat.engine.resources.openstack.heat import none_resource
 from heat.engine import support
 
 
-class MonascaNotification(resource.Resource):
+class MonascaNotification(none_resource.NoneResource):
     """Heat Template Resource for Monasca Notification.
 
     A resource which is used to notificate if there is some alarm.
@@ -32,164 +26,20 @@ class MonascaNotification(resource.Resource):
     """
 
     support_status = support.SupportStatus(
-        version='22.0.0',
-        status=support.DEPRECATED,
-        message=_('Monasca project was marked inactive'),
+        version='25.0.0',
+        status=support.HIDDEN,
+        message=_('Monasca project was retired'),
         previous_status=support.SupportStatus(
-            version='7.0.0',
-            status=support.SUPPORTED,
+            version='22.0.0',
+            status=support.DEPRECATED,
+            message=_('Monasca project was marked inactive'),
             previous_status=support.SupportStatus(
-                version='5.0.0',
-                status=support.SUPPORTED
-                )))
-
-    default_client_name = 'monasca'
-
-    entity = 'notifications'
-
-    # NOTE(sirushti): To conform to the autoscaling behaviour in heat, we set
-    # the default period interval during create/update to 60 for webhooks only.
-    _default_period_interval = 60
-
-    NOTIFICATION_TYPES = (
-        EMAIL, WEBHOOK, PAGERDUTY
-    ) = (
-        'email', 'webhook', 'pagerduty'
-    )
-
-    PROPERTIES = (
-        NAME, TYPE, ADDRESS, PERIOD
-    ) = (
-        'name', 'type', 'address', 'period'
-    )
-
-    properties_schema = {
-        NAME: properties.Schema(
-            properties.Schema.STRING,
-            _('Name of the notification. By default, physical resource name '
-              'is used.'),
-            update_allowed=True
-        ),
-        TYPE: properties.Schema(
-            properties.Schema.STRING,
-            _('Type of the notification.'),
-            update_allowed=True,
-            required=True,
-            constraints=[constraints.AllowedValues(
-                NOTIFICATION_TYPES
-            )]
-        ),
-        ADDRESS: properties.Schema(
-            properties.Schema.STRING,
-            _('Address of the notification. It could be a valid email '
-              'address, url or service key based on notification type.'),
-            update_allowed=True,
-            required=True,
-            constraints=[constraints.Length(max=512)]
-        ),
-        PERIOD: properties.Schema(
-            properties.Schema.INTEGER,
-            _('Interval in seconds to invoke webhooks if the alarm state '
-              'does not transition away from the defined trigger state. A '
-              'value of 0 will disable continuous notifications. This '
-              'property is only applicable for the webhook notification '
-              'type and has default period interval of 60 seconds.'),
-            support_status=support.SupportStatus(version='7.0.0'),
-            update_allowed=True,
-            constraints=[constraints.AllowedValues([0, 60])]
-        )
-    }
-
-    def _period_interval(self):
-        period = self.properties[self.PERIOD]
-        if period is None:
-            period = self._default_period_interval
-        return period
-
-    def validate(self):
-        super(MonascaNotification, self).validate()
-        if self.properties[self.PERIOD] is not None and (
-                self.properties[self.TYPE] != self.WEBHOOK):
-            msg = _('The period property can only be specified against a '
-                    'Webhook Notification type.')
-            raise exception.StackValidationFailed(message=msg)
-
-        address = self.properties[self.ADDRESS]
-        if not address:
-            return
-
-        if self.properties[self.TYPE] == self.WEBHOOK:
-            try:
-                parsed_address = parse.urlparse(address)
-            except Exception:
-                msg = _('Address "%(addr)s" should have correct format '
-                        'required by "%(wh)s" type of "%(type)s" '
-                        'property') % {
-                    'addr': address,
-                    'wh': self.WEBHOOK,
-                    'type': self.TYPE}
-                raise exception.StackValidationFailed(message=msg)
-            if not parsed_address.scheme:
-                msg = _('Address "%s" doesn\'t have required URL '
-                        'scheme') % address
-                raise exception.StackValidationFailed(message=msg)
-            if not parsed_address.netloc:
-                msg = _('Address "%s" doesn\'t have required network '
-                        'location') % address
-                raise exception.StackValidationFailed(message=msg)
-            if parsed_address.scheme not in ['http', 'https']:
-                msg = _('Address "%(addr)s" doesn\'t satisfies '
-                        'allowed schemes: %(schemes)s') % {
-                    'addr': address,
-                    'schemes': ', '.join(['http', 'https'])
-                }
-                raise exception.StackValidationFailed(message=msg)
-        elif (self.properties[self.TYPE] == self.EMAIL and
-              not re.match(r'^\S+@\S+$', address)):
-            msg = _('Address "%(addr)s" doesn\'t satisfies allowed format for '
-                    '"%(email)s" type of "%(type)s" property') % {
-                'addr': address,
-                'email': self.EMAIL,
-                'type': self.TYPE}
-            raise exception.StackValidationFailed(message=msg)
-
-    def handle_create(self):
-        args = dict(
-            name=(self.properties[self.NAME] or
-                  self.physical_resource_name()),
-            type=self.properties[self.TYPE],
-            address=self.properties[self.ADDRESS],
-        )
-        if args['type'] == self.WEBHOOK:
-            args['period'] = self._period_interval()
-
-        notification = self.client().notifications.create(**args)
-        self.resource_id_set(notification['id'])
-
-    def handle_update(self, json_snippet, tmpl_diff, prop_diff):
-        args = dict(notification_id=self.resource_id)
-
-        args['name'] = (prop_diff.get(self.NAME) or
-                        self.properties[self.NAME])
-
-        args['type'] = (prop_diff.get(self.TYPE) or
-                        self.properties[self.TYPE])
-
-        args['address'] = (prop_diff.get(self.ADDRESS) or
-                           self.properties[self.ADDRESS])
-
-        if args['type'] == self.WEBHOOK:
-            updated_period = prop_diff.get(self.PERIOD)
-            args['period'] = (updated_period if updated_period is not None
-                              else self._period_interval())
-
-        self.client().notifications.update(**args)
-
-    def handle_delete(self):
-        if self.resource_id is not None:
-            with self.client_plugin().ignore_not_found:
-                self.client().notifications.delete(
-                    notification_id=self.resource_id)
+                version='7.0.0',
+                status=support.SUPPORTED,
+                previous_status=support.SupportStatus(
+                    version='5.0.0',
+                    status=support.SUPPORTED
+                ))))
 
 
 def resource_mapping():
