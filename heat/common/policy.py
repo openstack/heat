ad811fc6c6f1281c8ad96b7f22983c18ff60daa9
Ghanshyam Mann <gmann@ghanshyammann.com>
2024-08-09 11:59:39 -0700

    Remove default override for RBAC config options
    RBAC config options enforce_scope and enforce_new_defaults
    were disabled by default in oslo.policy and Heat had to override
    the default value to enable those by default. Now oslo.policy
    (4.4.0 onwards[1]) changed the default values[2] and enabled
    by default for all the services. OpenStack service does not need
    to override the default anymore.
    
    NOTE: There is no change in behaviour here, oslo.policy provides the
    same configuration that Heat has overridden till now.
    
    [1] https://review.opendev.org/c/openstack/releases/+/925032
    [2] https://review.opendev.org/c/openstack/oslo.policy/+/924283
    
    Depends-On: https://review.opendev.org/c/openstack/requirements/+/925464
    Change-Id: If83eb095a8b68f91fc83b023c82bee57895125fe

--- heat/common/policy.py
+++ heat/common/policy.py
@@ -35,14 +35,12 @@ DEFAULT_RULES = policy.Rules.from_dict({'default': '!'})
 DEFAULT_RESOURCE_RULES = policy.Rules.from_dict({'default': '@'})
 
 # TODO(gmann): Remove setting the default value of config policy_file
-# once oslo_policy change the default value to 'policy.yaml'.
-# https://github.com/openstack/oslo.policy/blob/a626ad12fe5a3abd49d70e3e5b95589d279ab578/oslo_policy/opts.py#L49
+# 'policy_file' once oslo_policy change its default value to what
+# is overridden here.
 DEFAULT_POLICY_FILE = 'policy.yaml'
 opts.set_defaults(
     CONF,
-    DEFAULT_POLICY_FILE,
-    enforce_scope=True,
-    enforce_new_defaults=True)
+    DEFAULT_POLICY_FILE)
 
 ENFORCER = None
 
