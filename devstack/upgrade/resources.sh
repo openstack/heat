f4495e692f1a5864c6b1d547ed3453fbdd3f8e3f
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2025-11-04 16:41:47 +0000

    Use authentication with keystone ec2token api
    As a result of fixing OSSA-2025-002, ec2tokens API in Keystone
    now by default requires authentication.
    
    The config section `[ec2authtoken]` is now expected to have
    auth information for Heat to be able to use this API.
    
    In multicloud configuration keystone auth credentials are
    required for each cloud. These can be configured using the new clouds
    option and the ``[ec2authtoken.{cloud}]`` sections.
    
    NOTE:
     Disables test_software_config.ParallelDeploymentsTest in
     grenade. Should follow up to re-enable once fix has been
     backported.
    
    keystone change Ic84b84247e05f29874e2c5636a033aaedd4de83c has been
    backported to 2023.1
    
    Also pin setup tools to avoid ModuleNotFoundError: No module named
    'pkg_resources' error.
    
    Conflicts:
    	.pre-commit-config.yaml
    
    (2025.1 to 2024.2)
    Conflicts:
    	devstack/lib/heat
    
    Related-Bug: #2119646
    Change-Id: Ib41f76c1ba56005b6c4233424cca5768657a7686
    Co-Authored-By: Adrian Jarvis <adrian.jarvis@catalystcloud.nz>
    Co-Authored-By: Takashi Kajinami <kajinamit@oss.nttdata.com>
    Signed-off-by: Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
    (cherry picked from commit 0242c28d5f004591331bf7d005d7b305e555f398)
    (cherry picked from commit 76bf87551fdf3e1b9c82ae66c9ff22f82f70c01c)
    (cherry picked from commit 006357801e76a5a30da592becd08f90c1c4deb44)
    (cherry picked from commit 50c081b7a5747c3c76e10f77790a159860d977e6)
    Signed-off-by: rabi <ramishra@redhat.com>

--- devstack/upgrade/resources.sh
+++ devstack/upgrade/resources.sh
@@ -36,6 +36,9 @@ function _heat_set_user {
     OS_PROJECT_DOMAIN_ID=$DEFAULT_DOMAIN
 }
 
+# TODO: Add this test back once https://review.opendev.org/966092
+# has landed and backported.
+# heat_tempest_plugin.tests.functional.test_software_config.ParallelDeploymentsTest
 function _write_heat_integrationtests {
     local upgrade_tests=$1
     cat > $upgrade_tests <<EOF
@@ -46,7 +49,6 @@ heat_integrationtests.functional.test_resource_group.ResourceGroupTest
 heat_integrationtests.functional.test_resource_group.ResourceGroupUpdatePolicyTest
 heat_integrationtests.functional.test_software_deployment_group
 heat_integrationtests.functional.test_validation
-heat_tempest_plugin.tests.functional.test_software_config.ParallelDeploymentsTest
 heat_tempest_plugin.tests.functional.test_nova_server_networks
 EOF
 }
